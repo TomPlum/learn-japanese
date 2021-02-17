e67b2ec7ed0dd0ed57ba871725558c495fc14f8e
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2021-02-17 22:07:29 +0000

    Setup | Renamed App.tsx & Added TS Config

--- src/LearnJapanese.tsx
+++ src/LearnJapanese.tsx
@@ -1,13 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function LearnJapanese() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/LearnJapanese.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -22,4 +22,4 @@ function App() {
   );
 }
 
-export default App;
+export default LearnJapanese;
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import LearnJapanese from './LearnJapanese';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <LearnJapanese />
   </React.StrictMode>,
   document.getElementById('root')
 );
