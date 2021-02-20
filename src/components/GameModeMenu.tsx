2026785ab54f94334c4866123a4a366a14774e35
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2021-02-20 19:32:01 +0000

    Styling | Improved Game Mode Button Selection / Hover Styling

--- src/components/GameModeMenu.tsx
+++ src/components/GameModeMenu.tsx
@@ -121,7 +121,7 @@ class GameModeMenu extends Component<GameModeMenuProps, GameModeMenuState> {
     private getSelectedModeDescription(): string {
         switch (this.state.selected) {
             case GameMode.ROMANJI: {
-                return "You're shown kana(s) on screen and you must enter the correct romanji. " +
+                return "You're shown kana on screen and you must enter the correct romanji. " +
                     "You're timed, but there's no rush and mistakes aren't punishing.";
             }
             case GameMode.TIME_ATTACK: {
