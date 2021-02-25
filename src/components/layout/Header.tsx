4c4ca937d49973df62177d999dbb7d39562edf06
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2021-02-25 20:17:29 +0000

    Components | Re-Introduced Navbar Toggle & Justified Search Icon

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,7 +1,7 @@
 import { Component } from "react";
-import { Form, FormControl, Nav, Navbar } from "react-bootstrap";
+import { Navbar } from "react-bootstrap";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircle, faSearch } from "@fortawesome/free-solid-svg-icons";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import styles from "../../styles/sass/components/layout/Header.module.scss";
 
 interface HeaderProps {
@@ -11,12 +11,22 @@ interface HeaderProps {
 class Header extends Component<HeaderProps> {
     render() {
         return (
-            <Navbar bg="dark" variant="dark" expand="lg">
+            <Navbar bg="dark" variant="dark" expand={undefined} sticky="top">
                 <Navbar.Brand className={styles.brand}>
                     <img src="logo.png" className={styles.logo} alt="logo"/>
                 </Navbar.Brand>
 
-                <FontAwesomeIcon className={styles.search} icon={faSearch} onClick={this.props.onLaunchSearch}/>
+                <Navbar.Toggle />
+
+                <Navbar.Collapse className="justify-content-end">
+                    <Navbar.Text>
+                        <FontAwesomeIcon
+                            className={styles.search}
+                            icon={faSearch}
+                            onClick={this.props.onLaunchSearch}
+                        />
+                    </Navbar.Text>
+                </Navbar.Collapse>
             </Navbar>
         );
     }
