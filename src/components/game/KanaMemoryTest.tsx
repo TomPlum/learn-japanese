fac1216d9551467869edd54de8970a6e644221e7
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2021-02-28 10:58:10 +0000

    Components | Extracted Romanji Question Component & Auto-Focused Romanji Input

--- src/components/game/KanaMemoryTest.tsx
+++ src/components/game/KanaMemoryTest.tsx
@@ -1,14 +1,9 @@
 import React, { Component } from "react";
 import { Kana } from "../../types/Kana";
-import { Button, Col, Container, Form, ProgressBar, Row } from "react-bootstrap";
-import KanaDisplay from "./KanaDisplay";
+import { Col, Container, ProgressBar, Row } from "react-bootstrap";
 import styles from "../../styles/sass/components/game/KanaMemoryTest.module.scss";
 import { RandomNumberGenerator } from "../../utility/RandomNumberGenerator";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Timer from "./Timer";
-import { faRedoAlt } from "@fortawesome/free-solid-svg-icons";
-import HintButton from "./HintButton";
-import RomanjiInput from "./RomanjiInput";
 import { GameSettings } from "../../types/GameSettings";
 import LifeDisplay from "./LifeDisplay";
 import { LifeQuantity } from "../../types/LifeQuantity";
@@ -16,6 +11,7 @@ import QuitButton from "../ui/QuitButton";
 import GameResult from "../../types/GameResult";
 import { FailureReason } from "../../types/FailureReason";
 import CountDown from "./CountDown";
+import RomanjiQuestion from "./RomanjiQuestion";
 
 export interface KanaMemoryTestProps {
     kana: Kana[];
@@ -26,29 +22,24 @@ export interface KanaMemoryTestProps {
 
 interface KanaMemoryTestState {
     currentKana: Kana;
-    answer: string | undefined;
     remainingKana: Kana[];
     correctAnswers: Set<Kana>;
     wrongAnswers: Kana[];
     hasExhaustedKana: boolean;
     paused: boolean;
     lives: number;
-    hints: number;
-    hasUsedHintThisKana: boolean;
     failedToAnswer: number;
 }
 
 class KanaMemoryTest extends Component<KanaMemoryTestProps, KanaMemoryTestState> {
     private readonly timer: React.RefObject<Timer>;
     private readonly countdown: React.RefObject<CountDown>;
-    private readonly kanaDisplay: React.RefObject<KanaDisplay>;
 
     constructor(props: KanaMemoryTestProps | Readonly<KanaMemoryTestProps>) {
         super(props);
 
         this.timer = React.createRef();
         this.countdown = React.createRef();
-        this.kanaDisplay = React.createRef();
 
         const { settings, kana } = this.props;
 
@@ -59,12 +50,9 @@ class KanaMemoryTest extends Component<KanaMemoryTestProps, KanaMemoryTestState>
             remainingKana: remainingKana,
             correctAnswers: new Set<Kana>(),
             wrongAnswers: [],
-            answer: undefined,
             hasExhaustedKana: false,
             paused: false,
             lives: settings.lives.quantity?.valueOf() ?? LifeQuantity.ZERO,
-            hints: settings.hints.quantity?.valueOf() ?? 0,
-            hasUsedHintThisKana: false,
             failedToAnswer: 0
         }
     }
@@ -87,7 +75,7 @@ class KanaMemoryTest extends Component<KanaMemoryTestProps, KanaMemoryTestState>
     }
 
     render() {
-        const { currentKana, answer, correctAnswers, failedToAnswer, hasExhaustedKana, paused, lives, hints } = this.state;
+        const { currentKana, correctAnswers, failedToAnswer, hasExhaustedKana, paused, lives, remainingKana } = this.state;
         const { kana, settings } = this.props;
 
         return (
@@ -99,14 +87,18 @@ class KanaMemoryTest extends Component<KanaMemoryTestProps, KanaMemoryTestState>
                             className={styles.progress}
                             now={((correctAnswers.size + failedToAnswer) / kana.length) * 100}
                             variant={hasExhaustedKana ? "success" : undefined}
+                            title={(kana.length - remainingKana.length)  + "/" + kana.length}
                         />
                     </Col>
+
                     <Col>
                         <QuitButton onClick={this.close} />
                     </Col>
+
                     <Col className={styles.lifeDisplayContainer}>
                         {settings.lives.enabled && <LifeDisplay hearts={lives} />}
                     </Col>
+
                     <Col>
                         {settings.time.timed &&
                             <Timer className={styles.timer} ref={this.timer} pausable onPaused={this.onPaused}/>
@@ -122,53 +114,22 @@ class KanaMemoryTest extends Component<KanaMemoryTestProps, KanaMemoryTestState>
                     </Col>
                 </Row>
 
-                <KanaDisplay kana={currentKana} key={currentKana.code} ref={this.kanaDisplay} blur={paused}/>
-
-                <Form>
-                    <Form.Group controlId="answer">
-                        <Row>
-                            <Col xs={true} className={styles.inputCol}>
-                                <RomanjiInput
-                                    value={answer}
-                                    disabled={hasExhaustedKana || paused}
-                                    onChange={(e) => this.setState({ answer: e.target.value })}
-                                    placeholder={paused ? "Paused" : !hasExhaustedKana ? "Enter the romanji" : ""}
-                                    onEnterKey={this.answerQuestion}
-                                />
-                            </Col>
-                            <Col xs="auto" className={styles.tipCol}>
-                                <HintButton
-                                    kana={currentKana}
-                                    quantity={hints}
-                                    totalQuantity={settings.hints?.quantity?.valueOf() ?? 0}
-                                    key={currentKana.code}
-                                    title="Get a Hint"
-                                    disabled={paused || !settings.hints.enabled}
-                                    onUse={() => this.setState({ hasUsedHintThisKana: true })}
-                                />
-                            </Col>
-                        </Row>
-                    </Form.Group>
-
-                    <Button
-                        className={styles.submit}
-                        variant={!hasExhaustedKana ? "success" : "primary"}
-                        type="button"
-                        disabled={!answer && !hasExhaustedKana || paused}
-                        onClick={!hasExhaustedKana ? this.answerQuestion : this.reset}
-                    >
-                        {!hasExhaustedKana ? "Check" : <><FontAwesomeIcon icon={faRedoAlt}/> Restart</>}
-                    </Button>
-                </Form>
+                <RomanjiQuestion
+                    kana={currentKana}
+                    key={currentKana.code}
+                    onSubmit={this.answerQuestion}
+                    hidden={paused}
+                    hintSettings={settings.hints}
+                />
             </Container>
         );
     }
 
-    answerQuestion = () => {
-        const { currentKana, correctAnswers, wrongAnswers, answer, remainingKana, lives, hints, hasUsedHintThisKana } = this.state;
+    answerQuestion = (correct: boolean) => {
+        const { currentKana, correctAnswers, wrongAnswers, remainingKana, lives } = this.state;
         const { settings } = this.props;
 
-        if (answer === currentKana.romanji) {
+        if (correct) {
             //Add the current kana to the correct answers set.
             this.setState({ correctAnswers: correctAnswers.add(currentKana)});
 
@@ -184,22 +145,15 @@ class KanaMemoryTest extends Component<KanaMemoryTestProps, KanaMemoryTestState>
                 const [nextKana, nextRemainingKana] = this.getRandomKana(remainingKana);
 
                 //Update the next kana to be displayed and the remaining kana with one less.
-                this.setState({
-                    currentKana: nextKana,
-                    remainingKana: nextRemainingKana,
-                    hasUsedHintThisKana: false,
-                    hints: hasUsedHintThisKana ? hints - 1 : hints
-                });
+                this.setState({ currentKana: nextKana, remainingKana: nextRemainingKana });
             }
         } else {
-            this.kanaDisplay.current?.notifyIncorrect();
+            //If the question was answered incorrectly, update the lives and wrong answer pool.
             this.setState({
                 lives: settings.lives.enabled && !settings.time.countdown ? lives - 1 : lives,
                 wrongAnswers: wrongAnswers.concat(currentKana)
             });
         }
-
-        this.setState({ answer: "" });
     }
 
     reset = () => {
@@ -208,7 +162,6 @@ class KanaMemoryTest extends Component<KanaMemoryTestProps, KanaMemoryTestState>
         this.setState({
             currentKana: nextKana,
             remainingKana: remainingKana,
-            answer: "",
             correctAnswers: new Set<Kana>(),
             wrongAnswers: [],
             paused: false,
@@ -220,8 +173,8 @@ class KanaMemoryTest extends Component<KanaMemoryTestProps, KanaMemoryTestState>
     }
 
     private countDownTimeElapsed = () => {
-        const { lives, wrongAnswers, currentKana, remainingKana, hasUsedHintThisKana, hints, failedToAnswer } = this.state;
-        this.kanaDisplay.current?.notifyIncorrect();
+        const { lives, wrongAnswers, currentKana, remainingKana, failedToAnswer } = this.state;
+        //this.kanaDisplay.current?.notifyIncorrect(); TODO: Can we notify the question components of incorrectness when timer runs out?
         this.countdown.current?.reset();
 
         //Pick a random remaining kana and remove it from the pool.
@@ -230,11 +183,8 @@ class KanaMemoryTest extends Component<KanaMemoryTestProps, KanaMemoryTestState>
         this.setState({
             currentKana: nextKana,
             remainingKana: nextRemainingKana,
-            hasUsedHintThisKana: false,
-            hints: hasUsedHintThisKana ? hints - 1 : hints,
             lives: this.props.settings.lives.enabled ? lives - 1 : lives,
             wrongAnswers: wrongAnswers.concat(currentKana),
-            answer: "",
             failedToAnswer: failedToAnswer + 1
         });
     }
---
+++ src/components/game/RomanjiQuestion.tsx
@@ -0,0 +1,108 @@
+import React, { Component } from "react";
+import KanaDisplay from "./KanaDisplay";
+import { Button, Col, Form, Row } from "react-bootstrap";
+import styles from "../../styles/sass/components/game/KanaMemoryTest.module.scss";
+import RomanjiInput from "./RomanjiInput";
+import HintButton from "./HintButton";
+import { Kana } from "../../types/Kana";
+import { HintSettings } from "../../types/GameSettings";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faRedoAlt } from "@fortawesome/free-solid-svg-icons";
+
+interface RomanjiQuestionProps {
+    kana: Kana;
+    hidden: boolean;
+    hintSettings: HintSettings;
+    onSubmit: (correct: boolean) => void;
+}
+
+interface RomanjiQuestionState {
+    answer: string;
+    hints: number;
+    hasUsedHintThisQuestion: boolean;
+}
+
+class RomanjiQuestion extends Component<RomanjiQuestionProps, RomanjiQuestionState> {
+
+    private readonly kanaDisplay: React.RefObject<KanaDisplay>;
+
+    constructor(props: Readonly<RomanjiQuestionProps> | RomanjiQuestionProps) {
+        super(props);
+
+        this.kanaDisplay = React.createRef();
+
+        this.state = {
+            answer: "",
+            hints: this.props.hintSettings?.quantity?.valueOf() ?? 0,
+            hasUsedHintThisQuestion: false
+        }
+    }
+
+    render() {
+        const { kana, hidden, hintSettings } = this.props;
+        const { answer, hints } = this.state;
+
+        return (
+            <>
+                <KanaDisplay kana={kana} key={kana.code} ref={this.kanaDisplay} blur={hidden}/>
+
+                <Form>
+                    <Form.Group controlId="answer">
+                        <Row>
+                            <Col xs={true} className={styles.inputCol}>
+                                <RomanjiInput
+                                    value={answer}
+                                    disabled={!kana || hidden}
+                                    onChange={(e) => this.setState({ answer: e.target.value })}
+                                    placeholder={hidden ? "Paused" : "Enter the romanji"}
+                                    onEnterKey={this.answer}
+                                />
+                            </Col>
+                            <Col xs="auto" className={styles.tipCol}>
+                                <HintButton
+                                    kana={kana}
+                                    quantity={hints}
+                                    totalQuantity={hintSettings.quantity?.valueOf() ?? 0}
+                                    key={kana.code}
+                                    title="Get a Hint"
+                                    disabled={hidden || !hintSettings.enabled}
+                                    onUse={() => this.setState({ hasUsedHintThisQuestion: true })}
+                                />
+                            </Col>
+                        </Row>
+                    </Form.Group>
+
+                    <Button
+                        className={styles.submit}
+                        variant={!!kana ? "success" : "primary"}
+                        type="button"
+                        disabled={!answer && !!kana || hidden}
+                        onClick={this.answer}
+                    >
+                        {!!kana ? "Check" : <><FontAwesomeIcon icon={faRedoAlt}/> Restart</>}
+                    </Button>
+                </Form>
+            </>
+        );
+    }
+
+    private answer = () => {
+        const { answer, hints, hasUsedHintThisQuestion } = this.state;
+        const { kana } = this.props;
+
+        if (answer === kana.romanji) {
+            this.props.onSubmit(true);
+            this.setState({
+                hasUsedHintThisQuestion: false,
+                hints: hasUsedHintThisQuestion ? hints - 1 : hints
+            });
+        } else {
+            this.kanaDisplay.current?.notifyIncorrect();
+            this.props.onSubmit(false);
+        }
+
+        this.setState({ answer: "" });
+    }
+}
+
+export default RomanjiQuestion;
