6c9a82400628f255b9742520be64156d52f11093
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2021-03-02 17:24:12 +0000

    Testing | Fixed Quit Button Test

--- src/tests/components/ui/QuitButton.test.tsx
+++ src/tests/components/ui/QuitButton.test.tsx
@@ -1,8 +1,8 @@
-import QuitButton, { QuitButtonProps } from "../../../components/ui/QuitButton";
+import QuitButton from "../../../components/ui/QuitButton";
 import { fireEvent, render, screen } from "@testing-library/react";
 import React from 'react';
 
-test('Quit Button', () => {
+test('Clicking the Quit Button should call the onClick event handler', () => {
     const handleClick = jest.fn();
     render(<QuitButton onClick={handleClick} />);
 
