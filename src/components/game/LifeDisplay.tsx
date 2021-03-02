d29ae12e88e964db0f19f7d632851b754dadce46
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2021-03-02 10:32:54 +0000

    Components | Life Display - Updated Infinite Lives Icon

--- src/components/game/LifeDisplay.tsx
+++ src/components/game/LifeDisplay.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import { faHeart, faInfinity } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styles from "../../styles/sass/components/game/LifeDisplay.module.scss";
 
@@ -9,10 +9,13 @@ interface LifeDisplayProps {
 
 class LifeDisplay extends Component<LifeDisplayProps> {
     render() {
+        const { hearts } = this.props;
         return (
             <>
-                <FontAwesomeIcon icon={faHeart} className={styles.icon} />
-                <span className={styles.quantity}>{this.props.hearts}</span>
+                <FontAwesomeIcon icon={faHeart} className={styles.icon}/>
+                <span className={styles.quantity}>
+                    {hearts === 999 ? <FontAwesomeIcon icon={faInfinity}/> : hearts}
+                </span>
             </>
         );
     }
