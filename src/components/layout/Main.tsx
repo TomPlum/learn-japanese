f237c0474f21e780593298f5b064616c1af58489
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2021-03-05 20:33:57 +0000

    Testing | Added Main Component Test Suite

--- src/components/layout/Main.tsx
+++ src/components/layout/Main.tsx
@@ -1,30 +1,16 @@
 import { Component } from "react";
 import { Container } from "react-bootstrap";
 import styles from "../../styles/sass/components/layout/Main.module.scss";
-import { GameSettings } from "../../types/GameSettings";
 import { Route } from "react-router-dom";
 import Switch from "react-bootstrap/Switch";
 import GamePage from "../game/GamePage";
 import Search from "./Search";
 import Landing from "./Landing";
 
-interface MainState {
-    settings?: GameSettings;
-}
-
-class Main extends Component<{}, MainState> {
-    constructor(props: {} | Readonly<{}>) {
-        super(props);
-        this.state = {
-            settings: undefined,
-        }
-    }
-
+class Main extends Component {
     render() {
-        const { settings } = this.state;
-
         return (
-            <Container className={settings ? styles.wrapperFullScreen : styles.wrapper}>
+            <Container className={styles.wrapper}>
                 <Switch className={styles.switch}>
                     <Route path="/" component={Landing} exact />
                     <Route path="/play" component={GamePage}/>
---
+++ src/tests/layout/Main.test.tsx
@@ -0,0 +1,29 @@
+import { render, screen } from "@testing-library/react";
+import Main from "../../components/layout/Main";
+import { Router } from 'react-router-dom'
+import { createMemoryHistory } from 'history'
+
+const setup = (route: string) => {
+    const history = createMemoryHistory();
+    history.push(route);
+    render(
+        <Router history={history}>
+            <Main/>
+        </Router>
+    );
+}
+
+test('Navigating to the root URI should route to the Landing page', () => {
+    setup('/');
+    expect(screen.getByText('Learn Japanese Kana')).toBeInTheDocument();
+});
+
+test('Navigating to the /play should route to the Game page', () => {
+    setup('/play');
+    expect(screen.getByText('Relaxed')).toBeInTheDocument();
+});
+
+test('Navigating to the /search should route to the Search page', () => {
+    setup('/search');
+    expect(screen.getByText('あ')).toBeInTheDocument();
+});
