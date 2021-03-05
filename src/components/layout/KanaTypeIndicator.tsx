fa05b80f0b21a28f983de65bff1323e7058c0e6f
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2021-03-05 20:09:48 +0000

    Testing | Added KanaTypeIndicator Component Test Suite

--- src/components/layout/KanaTypeIndicator.tsx
+++ src/components/layout/KanaTypeIndicator.tsx
@@ -2,7 +2,7 @@ import { faCircle } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styles from "../../styles/sass/components/layout/KanaTypeIndicator.module.scss";
 
-interface KanaTypeIndicatorProps {
+export interface KanaTypeIndicatorProps {
     title: string;
     className?: string;
 }
