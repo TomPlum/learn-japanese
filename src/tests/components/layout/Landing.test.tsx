8d829911eedbdd2cf8d931a0a9b08402b2a748b6
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2021-03-09 22:24:55 +0000

    Components | Extracted ParallaxBackground From Landing

--- src/tests/components/layout/Landing.test.tsx
+++ src/tests/components/layout/Landing.test.tsx
@@ -117,23 +117,3 @@ test('Hovering over the \'Katakana\' inspectable text in the description should 
     expect(screen.getByTitle('Katakana (カタカナ)'));
     expect(screen.getByText('Katakana Description'));
 });
-
-//TODO: Below tests don't seem to be updating state and re-rendering new kana when resizing.
-test('Viewing in a small viewport should render less than 214 background kana', () => {
-    window.resizeTo(375, 812); //iPhone X
-    setup();
-    //expect(screen.queryAllByTestId('background-kana')).toHaveLength(50);
-});
-
-test('Viewing in a larger viewport should render more than 214 background kana', () => {
-    window.resizeTo(1920, 1080);
-    setup();
-    //expect(screen.queryAllByTestId('background-kana')).toHaveLength(50);
-});
-
-test('Resizing the viewport should recalculate the number of background kana', () => {
-    window.resizeTo(1920, 1080);
-    setup();
-    window.resizeTo(375, 812); //iPhone X
-    //expect(screen.queryAllByTestId('background-kana')).toHaveLength(50);
-});
