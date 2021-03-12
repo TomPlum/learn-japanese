8b3229d2d70108dae3e4616bc2a44b4af16749b1
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2021-03-12 17:33:46 +0000

    Testing | Added Search Component Test Suite

---
+++ src/tests/layout/Search.test.tsx
@@ -0,0 +1,56 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import Search from "../../components/layout/Search";
+
+const setup = () => {
+    const component = render(<Search />);
+    return {
+        search: component.getByPlaceholderText('Enter the romaji'),
+        hiragana: component.getByTestId('hiragana-switch'),
+        katakana: component.getByTestId('katakana-switch'),
+        diagraphs: component.getByTestId('diagraphs-switch'),
+        diacriticals: component.getByTestId('diacriticals-switch'),
+        ...component
+    }
+}
+
+test('On mount should render all Kana', () => {
+    setup();
+    expect(screen.getByText('214 Results')).toBeInTheDocument();
+});
+
+test('Toggling the Hiragana button off should hide all Hiragana Kana from the screen', () => {
+    const { hiragana } = setup();
+    fireEvent.click(hiragana);
+    expect(screen.getByText('107 Results')).toBeInTheDocument();
+});
+
+test('Toggling the Katakana button off should hide all Katakana Kana from the screen', () => {
+    const { katakana } = setup();
+    fireEvent.click(katakana);
+    expect(screen.getByText('107 Results')).toBeInTheDocument();
+});
+
+test('Toggling the Diagraphs button off should hide all diagraphs from the screen', () => {
+    const { diagraphs } = setup();
+    fireEvent.click(diagraphs);
+    expect(screen.getByText('142 Results')).toBeInTheDocument();
+});
+
+test('Toggling the Diacriticals button off should hide all diacritical Kana from the screen', () => {
+    const { diacriticals } = setup();
+    fireEvent.click(diacriticals);
+    expect(screen.getByText('137 Results')).toBeInTheDocument();
+});
+
+test('Searching should reduce the kana to match the term', () => {
+    const { search } = setup();
+    fireEvent.change(search, { target: { value: 'a' }});
+    expect(screen.getByText('54 Results')).toBeInTheDocument();
+});
+
+test('Searching and toggling a filter should combine to further reduce the results', () => {
+    const { search, hiragana } = setup();
+    fireEvent.change(search, { target: { value: 'a' }});
+    fireEvent.click(hiragana);
+    expect(screen.getByText('27 Results')).toBeInTheDocument();
+});
