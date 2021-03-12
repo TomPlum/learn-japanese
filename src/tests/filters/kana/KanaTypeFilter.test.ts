2a16cd1a994f26126e2670652f7c2208ceaff8f7
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2021-03-12 10:57:07 +0000

    Testing | Added KanaTypeFilter Test Suite

---
+++ src/tests/filters/kana/KanaTypeFilter.test.ts
@@ -0,0 +1,25 @@
+import { Kana } from "../../../types/Kana";
+import KanaType from "../../../types/KanaType";
+import { KanaColumn } from "../../../types/KanaColumn";
+import KanaTypeFilter from "../../../filters/kana/KanaTypeFilter";
+
+describe("Kana Type Filter", () => {
+    const data = [
+        new Kana("え", ["e"], KanaType.HIRAGANA, KanaColumn.VOWEL, false),
+        new Kana("ア", ["a"], KanaType.KATAKANA, KanaColumn.VOWEL, false)
+    ];
+
+    it("Should retain all Kana with the given type when the include boolean is true", () => {
+        const response = new KanaTypeFilter(KanaType.HIRAGANA, true).apply(data);
+        expect(response).toStrictEqual([
+            new Kana("え", ["e"], KanaType.HIRAGANA, KanaColumn.VOWEL, false)
+        ]);
+    });
+
+    it("Should filter out all kana with the given type by default", () => {
+        const response = new KanaTypeFilter(KanaType.HIRAGANA).apply(data);
+        expect(response).toStrictEqual([
+            new Kana("ア", ["a"], KanaType.KATAKANA, KanaColumn.VOWEL, false)
+        ]);
+    });
+});
