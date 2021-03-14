e76bc9a207eb1105cd4c31da40d3b0d53b6643bf
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2021-03-14 14:51:57 +0000

    Testing | Fixed Failing HashRouter Tests

--- src/tests/components/layout/Main.test.tsx
+++ src/tests/components/layout/Main.test.tsx
@@ -1,6 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import Main from "../../../components/layout/Main";
 import { Environment } from "../../../utility/Environment";
+import { createHashHistory } from "history";
 
 beforeEach(() => {
     Environment.variable = jest.fn().mockReturnValue("landing page description");
@@ -12,25 +13,25 @@ test('Navigating to the root URI should route to the Landing page', () => {
 });
 
 test('Navigating to the /play should route to the Game page', () => {
-    window.history.pushState({}, 'Play', '/play');
+    createHashHistory({}).push('/play');
     render(<Main/>);
     expect(screen.getByText('Relaxed')).toBeInTheDocument();
 });
 
 test('Navigating to the /search should route to the Search page', () => {
-    window.history.pushState({}, 'Search', '/search');
+    createHashHistory({}).push('/search');
     render(<Main/>);
     expect(screen.getByText('あ')).toBeInTheDocument();
 });
 
 test('Navigating to the /help should route to the Help page', () => {
-    window.history.pushState({}, 'Help', '/help');
+    createHashHistory({}).push('/help');
     render(<Main/>);
     expect(screen.getByText('Frequently Asked Questions')).toBeInTheDocument();
 });
 
 test('Navigating to an unknown URI should route to the Not Found page', () => {
-    window.history.pushState({}, 'Not Found', '/nope');
+    createHashHistory({}).push('/nope');
     render(<Main/>);
     expect(screen.getByText('Nani!?')).toBeInTheDocument();
 });
