0a40d4fc3a08da0bc0e19192c867f911ac302715
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2021-03-19 18:32:04 +0000

    Testing | Added GameTypeMenuDropdownOption Component Test Suite

---
+++ src/tests/components/layout/GameTypeMenuDropdownOption.test.tsx
@@ -0,0 +1,32 @@
+import { fireEvent, render } from "@testing-library/react";
+import GameTypeMenuDropdownOption from "../../../components/layout/GameTypeMenuDropdownOption";
+import { GameType } from "../../../types/GameType";
+
+const onClickHandler = jest.fn();
+
+test('Clicking on the option should call the onClick event handler', () => {
+    const component = render(<GameTypeMenuDropdownOption type={GameType.KANA} selected={GameType.KANA} onClick={onClickHandler} />);
+    fireEvent.click(component.getByText('Hiragana & Katakana'));
+    expect(onClickHandler).toHaveBeenCalled();
+});
+
+test('The option should be active if the selected prop matches the options type', () => {
+    const { container } = render(<GameTypeMenuDropdownOption type={GameType.KANA} selected={GameType.KANA} onClick={onClickHandler} />);
+    expect(container.firstChild).toHaveClass('active');
+});
+
+test('The option should not be active if the selected prop doesn\'t match the options type', () => {
+    const { container } = render(<GameTypeMenuDropdownOption type={GameType.KANA} selected={GameType.NUMBERS} onClick={onClickHandler} />);
+    expect(container.firstChild).not.toHaveClass('active');
+});
+
+test('Wrapping a child component should append the \'icon\' class to it', () => {
+    const component  = render(
+        <GameTypeMenuDropdownOption type={GameType.KANA} selected={GameType.NUMBERS} onClick={onClickHandler}>
+            <p>Example</p>
+        </GameTypeMenuDropdownOption>
+    );
+    const child = component.getByText('Example');
+    expect(child).toBeInTheDocument();
+    expect(child).toHaveClass('icon');
+});
