bece5185510a6821a4bae3674088bad2cf986a91
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2021-03-29 17:01:21 +0100

    Components | Extracted StartButton Component

---
+++ src/components/ui/StartButton.tsx
@@ -0,0 +1,21 @@
+import { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlay } from "@fortawesome/free-solid-svg-icons";
+import { Button } from "react-bootstrap";
+import styles from "../../styles/sass/components/ui/StartButton.module.scss";
+
+export interface StartButtonProps {
+    onClick: () => void;
+}
+
+class StartButton extends Component<StartButtonProps> {
+    render() {
+        return (
+            <Button variant="success" className={styles.button} block onClick={this.props.onClick}>
+                <FontAwesomeIcon size="xs" icon={faPlay}/> Start
+            </Button>
+        );
+    }
+}
+
+export default StartButton;
