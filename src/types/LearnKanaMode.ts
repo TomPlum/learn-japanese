828d722880c65ddd1f512c3d9b30af739e67a93e
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2021-03-29 18:23:55 +0100

    Components | Implemented LearnKanaMenu

---
+++ src/types/LearnKanaMode.ts
@@ -0,0 +1,8 @@
+export enum LearnKanaMode {
+    HIRAGANA = "Hiragana",
+    KATAKANA = "Katakana",
+    DIACRITICALS = "Diacriticals",
+    DIAGRAPHS = "Diagraphs",
+    ALL = "All",
+    CUSTOM = "Custom"
+}
