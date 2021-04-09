6ff5c7ddd717952d2931cd9a93ed2fdc1eeb4040
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2021-04-09 17:01:58 +0100

    Testing | Improved Coverage

--- src/utility/Viewports.ts
+++ src/utility/Viewports.ts
@@ -18,8 +18,6 @@ export default class Viewports {
     }
 
     public static fromFlashCardWidth(width: number | null): Viewport {
-        if (!width) return Viewport.PHONE;
-
         if (width === 450) {
             return Viewport.PHONE;
         } else if (width === 550) {
