6fef7d742d7c96f20230bec74974e55b11e6e319
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2021-04-12 20:33:01 +0100

    House Keeping | Encapsulated FlashCard Face Properties -> Topic Class

--- src/types/Topic.ts
+++ src/types/Topic.ts
@@ -4,24 +4,33 @@ import LearnKanaMode from "./learn/mode/LearnKanaMode";
 import LearnKanjiMode from "./learn/mode/LearnKanjiMode";
 import LearnBasicsMode from "./learn/mode/LearnBasicsMode";
 import LearnCalendarMode from "./learn/mode/LearnCalendarMode";
+import { CardProps } from "../components/learn/FlashCard";
+import KanaFlashCardFront from "../components/learn/kana/KanaFlashCardFront";
+import KanaFlashCardBack from "../components/learn/kana/KanaFlashCardBack";
+import CalendarFlashCardFront from "../components/learn/calendar/CalendarFlashCardFront";
+import CalendarFlashCardBack from "../components/learn/calendar/CalendarFlashCardBack";
+import KanjiFlashCardFront from "../components/learn/kanji/KanjiFlashCardFront";
+import KanjiFlashCardBack from "../components/learn/kanji/KanjiFlashCardBack";
 
 export default class Topic {
     private readonly _name: string;
     private readonly _icon: IconDefinition;
     private readonly _modes: LearnMenuModes;
+    private readonly _cards: CardProps;
 
-    public static KANA = new Topic("Hiragana & Katakana", faFont, new LearnKanaMode());
-    public static NUMBERS = new Topic("Numbers & Counting", faYenSign, new LearnKanaMode());
-    public static KANJI = new Topic("Jōyō Kanji", faPaintBrush, new LearnKanjiMode());
-    public static BASICS = new Topic("Basics", faAppleAlt, new LearnBasicsMode());
-    public static CALENDAR = new Topic("Days & Months", faCalendarAlt, new LearnCalendarMode());
+    public static KANA = new Topic("Hiragana & Katakana", faFont, new LearnKanaMode(), { front: KanaFlashCardFront, back: KanaFlashCardBack });
+    public static NUMBERS = new Topic("Numbers & Counting", faYenSign, new LearnKanaMode(), { front: KanjiFlashCardFront, back: KanjiFlashCardBack });
+    public static KANJI = new Topic("Jōyō Kanji", faPaintBrush, new LearnKanjiMode(), { front: KanjiFlashCardFront, back: KanjiFlashCardBack });
+    public static BASICS = new Topic("Basics", faAppleAlt, new LearnBasicsMode(), { front: KanjiFlashCardFront, back: KanjiFlashCardBack });
+    public static CALENDAR = new Topic("Days & Months", faCalendarAlt, new LearnCalendarMode(), { front: CalendarFlashCardFront, back: CalendarFlashCardBack });
 
     public static ALL: Topic[] = [Topic.KANA, Topic.NUMBERS, Topic.KANJI, Topic.BASICS, Topic.CALENDAR];
 
-    private constructor(name: string, icon: IconDefinition, modes: LearnMenuModes) {
+    private constructor(name: string, icon: IconDefinition, modes: LearnMenuModes, cards: CardProps) {
         this._name = name;
         this._icon = icon;
         this._modes = modes;
+        this._cards = cards;
     }
 
     get name(): string {
@@ -34,5 +43,9 @@ export default class Topic {
 
     get modes(): LearnMenuModes {
         return this._modes;
+    }
+
+    get cards(): CardProps {
+        return this._cards;
     }
 }
