82ac493c49d8047a633512a0bdf09ad9313a9f13
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2021-07-02 17:13:03 +0100

    Game | Added Play Kanji Modes Configuration

---
+++ src/types/game/mode/PlayKanjiModes.ts
@@ -0,0 +1,23 @@
+import { faCircle, faClock, faFillDrip, faPaintBrush, faRandom, faSchool, faStar, faYenSign } from "@fortawesome/free-solid-svg-icons";
+import { RELAXED } from "../../../data/GameModePresets";
+import SessionMode from "../../SessionMode";
+import { MenuModes } from "../../MenuModes";
+import CustomLearningMode from "../../learn/CustomLearningMode";
+import KanjiSettingsMenu from "../../../components/learn/kanji/KanjiSettingsMenu";
+
+export default class PlayKanjiModes implements MenuModes {
+    getModes(): SessionMode[] {
+        return [
+            new SessionMode("Kanji", "#6857ee", faPaintBrush, RELAXED),
+            new SessionMode("Meaning", "#65cd3a", faSchool, RELAXED),
+            new SessionMode("On'yomi", "#eacd35", faStar, RELAXED),
+            new SessionMode("Kun'yomi", "#e21717", faCircle, RELAXED),
+            new SessionMode("Random", "#3cabca", faRandom, RELAXED),
+            new CustomLearningMode(KanjiSettingsMenu)
+        ];
+    }
+
+    getTopic(): string {
+        return "KANJI";
+    }
+}
