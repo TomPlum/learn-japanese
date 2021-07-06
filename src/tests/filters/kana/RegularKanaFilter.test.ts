725863d9668e1d2983ca048c16427889dc376027
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2021-07-06 17:26:13 +0100

    Testing | Added RegularKanaFilter Test Suite

---
+++ src/tests/filters/kana/RegularKanaFilter.test.ts
@@ -0,0 +1,25 @@
+import { Kana } from "../../../types/kana/Kana";
+import KanaType from "../../../types/kana/KanaType";
+import { KanaColumn } from "../../../types/kana/KanaColumn";
+import RegularKanaFilter from "../../../filters/kana/RegularKanaFilter";
+
+describe("Regular Kana Filter", () => {
+    const data = [
+        new Kana("しゅ", ["shu"], KanaType.HIRAGANA, KanaColumn.S, false),
+        new Kana("びゃ", ["bya"], KanaType.HIRAGANA, KanaColumn.H, true),
+        new Kana("ぎ", ["gi"], KanaType.HIRAGANA, KanaColumn.K, true),
+        new Kana("え", ["e"], KanaType.HIRAGANA, KanaColumn.VOWEL, false)
+    ];
+
+    it("Should filter out all regular kana (non-diacritical, non-diagraph) by default", () => {
+        const filter = new RegularKanaFilter();
+        const response = filter.apply(data);
+        expect(response.map((kana: Kana) => kana.code)).toStrictEqual(["しゅ", "びゃ", "ぎ"]);
+    });
+
+    it("Should keep only regular kana (non-diacritical, non-diagraph) when include is passed as true", () => {
+        const filter = new RegularKanaFilter(true);
+        const response = filter.apply(data);
+        expect(response.map((kana: Kana) => kana.code)).toStrictEqual(["え"]);
+    });
+});
