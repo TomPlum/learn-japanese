3cbda0b72e3363d86a5375a7a54f338a7a0abfb6
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2021-07-24 19:45:09 +0100

    House Keeping | Cleaned LearnTopicButton Test Suite

--- src/tests/components/learn/LearnTopicButton.test.tsx
+++ src/tests/components/learn/LearnTopicButton.test.tsx
@@ -1,68 +1,59 @@
-import { fireEvent, render, screen } from "@testing-library/react";
-import LearnTopicButton from "../../../components/learn/LearnTopicButton";
+import { fireEvent, render } from "@testing-library/react";
+import LearnTopicButton, { LearnTopicButtonProps } from "../../../components/learn/LearnTopicButton";
 import { faCircle } from "@fortawesome/free-solid-svg-icons";
 import LearnKanaModes from "../../../types/learn/mode/LearnKanaModes";
 
 const onClickHandler = jest.fn();
 
-test('Should accept string icon', () => {
-    render(
-        <LearnTopicButton
-            icon="ICON"
-            type={new LearnKanaModes().getModes()[0]}
-            selected={new LearnKanaModes().getModes()[0]}
-            onClick={onClickHandler}
-        />
+const mode = new LearnKanaModes().getModes()[0];
+
+let props: LearnTopicButtonProps;
+
+beforeEach(() => {
+    props = {
+        icon: "ICON",
+        type: mode,
+        selected: mode,
+        onClick: onClickHandler
+    }
+});
+
+const setup = () => {
+    const component = render(
+        <LearnTopicButton {...props} />
     );
-    expect(screen.getByText('ICON')).toBeInTheDocument();
-    expect(screen.getByText('Hiragana')).toBeInTheDocument();
+
+    return {
+        icon: component.getByText('Icon'),
+        button: component.getByText('Hiragana'),
+        ...component
+    }
+}
+
+test('Should accept string icon', () => {
+   const { button, icon } = setup();
+    expect(icon).toBeInTheDocument();
+    expect(button).toBeInTheDocument();
 });
 
 test('Should accept Font Awesome icon', () => {
-    render(
-        <LearnTopicButton
-            icon={faCircle}
-            type={new LearnKanaModes().getModes()[0]}
-            selected={new LearnKanaModes().getModes()[0]}
-            onClick={onClickHandler}
-        />
-    );
-    expect(screen.getByText('Hiragana')).toBeInTheDocument();
+    props.icon = faCircle;
+    const { button } = setup();
+    expect(button).toBeInTheDocument();
 });
 
 test('Should have the class \'selected\' when the passed type matches the selected prop', () => {
-    const { container } = render(
-        <LearnTopicButton
-            icon={faCircle}
-            type={new LearnKanaModes().getModes()[0]}
-            selected={new LearnKanaModes().getModes()[0]}
-            onClick={onClickHandler}
-        />
-    );
+    const { container } = setup();
     expect(container.firstChild).toHaveClass('selected');
 });
 
 test('Should have the class \'notSelected\' when the passed type does not match the selected prop', () => {
-    const { container } = render(
-        <LearnTopicButton
-            icon={faCircle}
-            type={new LearnKanaModes().getModes()[0]}
-            selected={new LearnKanaModes().getModes()[1]}
-            onClick={onClickHandler}
-        />
-    );
+    const { container } = setup();
     expect(container.firstChild).toHaveClass('notSelected');
 });
 
 test('Clicking the button should call the onClick event handler with the passed type', () => {
-    const component = render(
-        <LearnTopicButton
-            icon={faCircle}
-            type={new LearnKanaModes().getModes()[0]}
-            selected={new LearnKanaModes().getModes()[0]}
-            onClick={onClickHandler}
-        />
-    );
-    fireEvent.click(component.getByText('Hiragana'));
+    const { button } = setup();
+    fireEvent.click(button);
     expect(onClickHandler).toHaveBeenCalled();
 });
--- src/types/learn/mode/LearnKanaModes.ts
+++ src/types/learn/mode/LearnKanaModes.ts
@@ -4,7 +4,7 @@ import LearnMode from "../../session/LearnMode";
 import LearnSettings from "../../session/settings/LearnSettings";
 import { KanaSettingsBuilder } from "../../session/settings/data/KanaSettings";
 
-export default class LearnKanaModes implements LearnMenuModes {
+class LearnKanaModes implements LearnMenuModes {
     getModes(): LearnMode[] {
         const defaultLearnSettings = new LearnSettings();
 
@@ -52,3 +52,5 @@ export default class LearnKanaModes implements LearnMenuModes {
         return "KANA"
     }
 }
+
+export default LearnKanaModes;
