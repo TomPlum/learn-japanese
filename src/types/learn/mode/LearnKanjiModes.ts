5ba4545eb6fdb8634bd50d4684ee5af49e133fe4
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2021-08-01 17:11:26 +0100

    Config | Removed Custom Learn Kanji Mode & Added High School Preset

--- src/types/learn/mode/LearnKanjiModes.ts
+++ src/types/learn/mode/LearnKanjiModes.ts
@@ -1,6 +1,5 @@
-import { faClock, faFillDrip, faPaintBrush, faSchool, faYenSign } from "@fortawesome/free-solid-svg-icons";
+import { faChalkboardTeacher, faClock, faFillDrip, faPaintBrush, faSchool, faYenSign } from "@fortawesome/free-solid-svg-icons";
 import { KyoikuGrade } from "../../kanji/KyoikuGrade";
-import CustomLearningMode from "../CustomLearningMode";
 import { LearnMenuModes } from "../../MenuModes";
 import LearnMode from "../../session/LearnMode";
 import { KanjiSettingsBuilder } from "../../session/settings/data/KanjiSettings";
@@ -11,11 +10,11 @@ export default class LearnKanjiModes implements LearnMenuModes {
         const defaultLearnSettings = new LearnSettings();
         return [
             new LearnMode("Kyōiku", "#fdb40e", faSchool, new KanjiSettingsBuilder().withGrades(KyoikuGrade.ALL).build(), defaultLearnSettings),
+            new LearnMode("High School", "#74d43f", faChalkboardTeacher, new KanjiSettingsBuilder().withJoyoKanji(true).build(), defaultLearnSettings),
             new LearnMode("Jōyō", "#ff7730", faPaintBrush, new KanjiSettingsBuilder().withJoyoKanji(true).build(), defaultLearnSettings),
             new LearnMode("Numbers", "#1785e2", faYenSign, new KanjiSettingsBuilder().withTags(["number"]).build(), defaultLearnSettings),
             new LearnMode("Colours", "#a01219", faFillDrip, new KanjiSettingsBuilder().withTags(["colour"]).build(), defaultLearnSettings),
             new LearnMode("Time", "#fc3131", faClock, new KanjiSettingsBuilder().withTags(["time"]).build(), defaultLearnSettings),
-            new CustomLearningMode()
         ];
     }
 
