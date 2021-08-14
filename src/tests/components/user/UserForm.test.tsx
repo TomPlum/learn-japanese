d17fd2fc67a583ab9356dee56461a0d893a493fd
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2021-08-14 19:47:20 +0100

    Components | Implemented UserForm (Aggregated Login / Registration)

---
+++ src/tests/components/user/UserForm.test.tsx
@@ -0,0 +1,40 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import UserForm, { LoginRegistrationFormProps } from "../../../components/user/UserForm";
+
+let props: LoginRegistrationFormProps;
+
+const onCloseHandler = jest.fn();
+
+beforeEach(() => {
+    props = {
+        show: true,
+        location: "",
+        onClose: onCloseHandler
+    }
+});
+
+const setup = () => {
+    const component = render(<UserForm {...props} />);
+    return {
+        switchForm: component.getByText("I don't have an account"),
+        close: component.getByText('Close'),
+        ...component
+    }
+}
+
+test('Should render the login form by default', () => {
+    setup();
+    expect(screen.getAllByText('Login')[1]).toBeInTheDocument();
+});
+
+test('Clicking the "I don\'t have an account" button should switch to the registration form', () => {
+    const { switchForm } = setup();
+    fireEvent.click(switchForm);
+    expect(screen.getAllByText('Register')[1]).toBeInTheDocument();
+});
+
+test('Clicking the close button should call the onClose event handler', async () => {
+    const { close } = setup();
+    fireEvent.click(close);
+    expect(onCloseHandler).toHaveBeenCalled();
+});
