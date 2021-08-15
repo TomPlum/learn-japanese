47534a7c99578e5a1dc758d755aaaa2ce592981f
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2021-08-15 19:38:17 +0100

    Components | UserButton - Fixed Blue Shadow CSS Issue

--- src/components/user/UserButton.tsx
+++ src/components/user/UserButton.tsx
@@ -1,28 +1,31 @@
+import React, { ReactChildren } from "react";
 import { Dropdown, Nav } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChartBar, faDoorOpen, faUser, faUserCircle, faUserTie } from "@fortawesome/free-solid-svg-icons";
 import { useUserDispatch, useUserSelector } from "../../hooks";
-import React from "react";
-import parentStyles from "../../styles/sass/components/layout/ControlsMenu.module.scss";
-import styles from "../../styles/sass/components/user/UserButton.module.scss";
 import { clearUser } from "../../slices/UserSlice";
+import styles from "../../styles/sass/components/user/UserButton.module.scss";
+import menuStyles from "../../styles/sass/components/layout/ControlsMenu.module.scss";
 
 export interface UserButtonProps {
     onClick: () => void;
     disabled: boolean;
 }
 
-const UserButton = React.forwardRef((props: UserButtonProps, ref: React.ForwardedRef<HTMLAnchorElement>) => {
+const UserButton = React.forwardRef((props: UserButtonProps, ref: React.ForwardedRef<HTMLDivElement>) => {
     const userDispatch = useUserDispatch();
     const userSelector = useUserSelector(state => state.user);
     const user = userSelector.user;
+    console.log(user)
     const userNickname = user?.nickname;
     const username = user?.username;
 
-    const handleClick = (e: React.MouseEvent) => {
-        if (!user) {
-            props.onClick();
+    const handleClick = (e: React.MouseEvent, parentEvent: any) => {
+        if (user) {
+            parentEvent(e);
             e.preventDefault();
+        } else {
+            props.onClick();
             e.stopPropagation();
         }
         return false;
@@ -30,38 +33,42 @@ const UserButton = React.forwardRef((props: UserButtonProps, ref: React.Forwarde
 
     const disabled = props.disabled;
 
-    return (
-        <Nav.Link className={parentStyles.navLink} onClick={handleClick} disabled={disabled} ref={ref}>
-            <Dropdown>
-                <Dropdown.Toggle className={disabled ? styles.disabled : styles.toggle} bsPrefix="toggle">
-                    <div>
-                        <FontAwesomeIcon icon={user ? faUserTie : faUser} className={parentStyles.icon}/>
-                    </div>
-                    <span className={parentStyles.linkText}>
-                       {user ? userNickname ? user.nickname : username : 'Login'}
-                    </span>
-                </Dropdown.Toggle>
-
-                {user && (
-                    <Dropdown.Menu className={styles.dropdown}>
-                        <Dropdown.Item>
-                            <FontAwesomeIcon icon={faUserCircle} fixedWidth/>
-                            {' Profile'}
-                        </Dropdown.Item>
-
-                        <Dropdown.Item>
-                            <FontAwesomeIcon icon={faChartBar} fixedWidth/>
-                            {' Stats'}
-                        </Dropdown.Item>
-
-                        <Dropdown.Item onClick={() => userDispatch(clearUser())}>
-                            <FontAwesomeIcon icon={faDoorOpen} fixedWidth/>
-                            {' Logout'}
-                        </Dropdown.Item>
-                    </Dropdown.Menu>
-                )}
-            </Dropdown>
+    const CustomToggle = React.forwardRef((props: { children: ReactChildren, onClick: React.MouseEvent }, ref: React.Ref<HTMLAnchorElement>) => (
+        <Nav.Link ref={ref} onClick={(e: React.MouseEvent) => handleClick(e, props.onClick)} className={menuStyles.navLink}>
+            {props.children}
         </Nav.Link>
+    ));
+
+    return (
+        <Dropdown ref={ref}>
+            <Dropdown.Toggle as={CustomToggle} className={disabled ? styles.disabled : styles.toggle}>
+                <div>
+                    <FontAwesomeIcon icon={user ? faUserTie : faUser} className={menuStyles.icon}/>
+                </div>
+                <span className={menuStyles.linkText}>
+                   {user ? userNickname ? user.nickname : username : 'Login'}
+                </span>
+            </Dropdown.Toggle>
+
+            {user && (
+                <Dropdown.Menu className={styles.dropdown}>
+                    <Dropdown.Item>
+                        <FontAwesomeIcon icon={faUserCircle} fixedWidth/>
+                        {' Profile'}
+                    </Dropdown.Item>
+
+                    <Dropdown.Item>
+                        <FontAwesomeIcon icon={faChartBar} fixedWidth/>
+                        {' Stats'}
+                    </Dropdown.Item>
+
+                    <Dropdown.Item onClick={() => userDispatch(clearUser())}>
+                        <FontAwesomeIcon icon={faDoorOpen} fixedWidth/>
+                        {' Logout'}
+                    </Dropdown.Item>
+                </Dropdown.Menu>
+            )}
+        </Dropdown>
     );
 });
 
