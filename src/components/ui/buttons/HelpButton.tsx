e66e5cbfce59794771fb563d18b63e7489525de5
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2021-08-17 19:15:25 +0100

    Components | Refactored Navigation Bar Help Button & Added Placeholder Options

---
+++ src/components/ui/buttons/HelpButton.tsx
@@ -0,0 +1,24 @@
+import NavigationButton from "../NavigationButton";
+import { faBook, faInfoCircle, faPaintBrush, faQuestion } from "@fortawesome/free-solid-svg-icons";
+import menuStyles from "../../../styles/sass/components/layout/ControlsMenu.module.scss";
+import styles from "../../../styles/sass/components/ui/buttons/HelpButton.module.scss";
+
+const HelpButton = () => {
+    return (
+        <NavigationButton text="Help" icon={faInfoCircle} iconClass={menuStyles.icon} textClass={menuStyles.linkText} width={250}>
+            <NavigationButton.Item icon={faPaintBrush} href="/help" iconClass={styles.kanji}>
+                Jōyō Kanji Bank
+            </NavigationButton.Item>
+
+            <NavigationButton.Item icon={faBook} href="/help" iconClass={styles.genki}>
+                Genki Knowledge Bank
+            </NavigationButton.Item>
+
+            <NavigationButton.Item icon={faQuestion} href="/help" iconClass={styles.faq}>
+                Frequently Asked Questions
+            </NavigationButton.Item>
+        </NavigationButton>
+    );
+}
+
+export default HelpButton;
