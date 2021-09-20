39a3e65025f7dbb736404ec3135af4719b11b7cb
WillPIum <will.plum@hotmail.com>
2021-09-20 19:00:32 +0100

    Testing | Extracted Commands & Increased View Port Size

--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -1,5 +1,9 @@
 declare namespace Cypress {
     interface Chainable {
-       class(className: string): Chainable<any>
+       class(className: string): Chainable<any>;
+       getByTitle(titleName: string): Chainable<any>;
+       startGame(presetName: string, topicName?: string): Chainable<any>;
+       startAndQuit(presetName: string, topicName?: string): Chainable<any>;
+       navigateToMenu(): Chainable<any>;
     }
 }
