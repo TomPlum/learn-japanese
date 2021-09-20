4400aaf5e256d1e1c834aa8cd9cb02343a458715
WillPIum <will.plum@hotmail.com>
2021-09-20 19:15:42 +0100

    Testing | Extracted Topic Enum

---
+++ cypress/support/Topic.ts
@@ -0,0 +1,8 @@
+export enum Topic {
+    KANA = 'Hiragana & Katakana',
+    NUMBERS = 'Numbers & Counting',
+    KANJI = 'Jōyō Kanji',
+    BASICS = 'Basics',
+    DAYS = 'Days & Months',
+    SENTENCE = 'Sentence Structure'
+}
--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -1,9 +1,11 @@
+import {Topic} from "./Topic";
+
 declare namespace Cypress {
     interface Chainable {
        class(className: string): Chainable<any>;
        getByTitle(titleName: string): Chainable<any>;
-       startGame(presetName: string, topicName?: string): Chainable<any>;
-       startAndQuit(presetName: string, topicName?: string): Chainable<any>;
+       startGame(presetName: string, topicName?: Topic): Chainable<any>;
+       startAndQuit(presetName: string, topicName?: Topic): Chainable<any>;
        navigateToMenu(): Chainable<any>;
     }
 }
