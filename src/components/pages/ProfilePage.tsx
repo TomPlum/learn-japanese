ec3cd2d69403c7c635fbc964f1cdc48f52b94563
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2021-09-25 07:32:36 +0100

    Pages | Added Profile Page Example Placeholder Cards

--- src/components/pages/ProfilePage.tsx
+++ src/components/pages/ProfilePage.tsx
@@ -1,8 +1,8 @@
 import { useUserSelector } from "../../hooks";
 import { Redirect } from "react-router-dom";
-import { Col, Container, FormControl, Row, Tab, Tabs } from "react-bootstrap";
+import { Button, Card, CardColumns, Col, Container, FormControl, Row, Table } from "react-bootstrap";
 import styles from "../../styles/sass/components/pages/ProfilePage.module.scss";
-import { faCheckCircle, faCircleNotch, faPencilAlt } from "@fortawesome/free-solid-svg-icons";
+import { faCheckCircle, faCircleNotch, faPencilAlt, faRedo } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { ChangeEvent, useState } from "react";
 import dayjs from 'dayjs';
@@ -17,7 +17,7 @@ const ProfilePage = () => {
     const [nickname, setNickname] = useState(user?.nickname);
 
     if (!user) {
-        return <Redirect to={"/menu"} />;
+        return <Redirect to={"/menu"}/>;
     }
 
     const onClickEdit = () => {
@@ -43,74 +43,178 @@ const ProfilePage = () => {
         return `${day}${suffix} ${date.format('MMMM')} ${date.year()}`
     }
 
+    const onRefreshHighscores = () => {
+
+    }
+
     return (
         <Container fluid className={styles.wrapper}>
-            <Row>
-                <Col className={styles.sidebar} xs={12} md={4}>
-                    <h2 className={styles.heading}>
-                        About
-                        {editing ? <FontAwesomeIcon
-                            size="sm"
-                            title="Save"
-                            icon={faCheckCircle}
-                            className={styles.save}
-                            onClick={onFinishEditing}
-                        /> : saving ?
-                        <FontAwesomeIcon
-                            size="sm"
-                            spin={true}
-                            title="Saving..."
-                            icon={faCircleNotch}
-                            className={styles.spinner}
-                        /> :
-                        <FontAwesomeIcon
-                            size="sm"
-                            title="Edit"
-                            icon={faPencilAlt}
-                            onClick={onClickEdit}
-                            className={styles.edit}
-                        />}
-                    </h2>
+            <CardColumns>
+                <Card className={styles.card} border="primary">
+                    <Card.Body>
+                        <h2 className={styles.heading}>
+                            About
+                            {editing ? <FontAwesomeIcon
+                                size="sm"
+                                title="Save"
+                                icon={faCheckCircle}
+                                className={styles.save}
+                                onClick={onFinishEditing}
+                            /> : saving ?
+                                <FontAwesomeIcon
+                                    size="sm"
+                                    spin={true}
+                                    title="Saving..."
+                                    icon={faCircleNotch}
+                                    className={styles.spinner}
+                                /> :
+                                <FontAwesomeIcon
+                                    size="sm"
+                                    title="Edit"
+                                    icon={faPencilAlt}
+                                    onClick={onClickEdit}
+                                    className={styles.edit}
+                                />}
+                        </h2>
 
-                    <p className={styles.label}>Joined</p>
-                    <p className={styles.value}>{parseDate()}</p>
+                        <p className={styles.label}>Joined</p>
+                        <p className={styles.value}>{parseDate()}</p>
 
-                    <p className={styles.label}>Username</p>
-                    <p className={styles.value}>{user.username}</p>
+                        <p className={styles.label}>Username</p>
+                        <p className={styles.value}>{user.username}</p>
 
-                    <p className={styles.label}>Nickname</p>
-                    {editing ?
-                        <FormControl value={nickname} onChange={onNicknameChange} placeholder="Nickname" /> :
-                        <p className={styles.value}>{nickname}</p>
-                    }
+                        <p className={styles.label}>Nickname</p>
+                        {editing ?
+                            <FormControl value={nickname} onChange={onNicknameChange} placeholder="Nickname"/> :
+                            <p className={styles.value}>{nickname}</p>
+                        }
 
-                    <p className={styles.label}>Email</p>
-                    <p className={styles.value}>{user.email}</p>
-                </Col>
+                        <p className={styles.label}>Email</p>
+                        <p className={styles.value}>{user.email}</p>
+                    </Card.Body>
+                </Card>
 
-                <Col xs={12} md={7} className={styles.main}>
-                    <Tabs defaultActiveKey="overview" id="profile-tabs" className="mb-3" variant="pills">
-                        <Tab title="Overview" eventKey="overview">
+                <Card className={styles.card}>
+                    <Card.Body>
+                        <h2 className={styles.heading}>Overview</h2>
+                    </Card.Body>
+                </Card>
 
-                        </Tab>
+                <Card className={styles.card} border="warning">
+                    <Card.Body>
+                        <h2 className={styles.heading}>
+                            Ranks
+                            <FontAwesomeIcon
+                                size="sm"
+                                title="Refresh"
+                                icon={faRedo}
+                                className={styles.refresh}
+                                onClick={onRefreshHighscores}
+                            />
+                        </h2>
+                        <Table responsive hover>
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Leaderboard</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>2</td>
+                                    <td>Romaji Time Attack</td>
+                                </tr>
+                                <tr>
+                                    <td>16</td>
+                                    <td>Kanji Hardcore</td>
+                                </tr>
+                            </tbody>
+                        </Table>
+                    </Card.Body>
+                </Card>
 
-                        <Tab title="Stats" eventKey="stats">
+                <Card className={styles.card}>
+                    <Card.Body>
+                        <h2 className={styles.heading}>Stats</h2>
+                    </Card.Body>
+                </Card>
 
-                        </Tab>
+                <Card className={styles.card}>
+                    <Card.Body>
+                        <h2 className={styles.heading}>Preferences</h2>
+                        <Row>
+                            <Col xs={6}>
+                                <p className={styles.label}>Default font</p>
+                            </Col>
+                            <Col xs={6}>
+                                <p className={styles.value}>Default font</p>
+                            </Col>
+                        </Row>
 
-                        <Tab title="Kanji" eventKey="kanji">
+                        <Row>
+                            <Col xs={6}>
+                                <p className={styles.label}>Theme</p>
+                            </Col>
+                            <Col xs={6}>
+                                <p className={styles.value}>Dark Mode</p>
+                            </Col>
+                        </Row>
 
-                        </Tab>
+                        <Row>
+                            <Col xs={6}>
+                                <p className={styles.label}>Language</p>
+                            </Col>
+                            <Col xs={6}>
+                                <p className={styles.value}>English</p>
+                            </Col>
+                        </Row>
 
-                        <Tab title="Ranks" eventKey="ranks">
-                            <p>You are not current ranked on any high scores.</p>
-                        </Tab>
-                    </Tabs>
-                </Col>
-            </Row>
+                        <Row>
+                            <Col xs={6}>
+                                <p className={styles.label}>Highscores</p>
+                            </Col>
+                            <Col xs={6}>
+                                <p className={styles.value}>Auto-Submit</p>
+                            </Col>
+                        </Row>
+                    </Card.Body>
+                </Card>
+
+                <Card className={styles.card} border="danger">
+                    <Card.Body>
+                        <h2 className={styles.heading}>Danger Zone</h2>
+
+                        <Row>
+                            <Col xs={6}>
+                                <p className={styles.label}>Clear Local Storage</p>
+                            </Col>
+                            <Col xs={6}>
+                                <Button variant="danger">Clear</Button>
+                            </Col>
+                        </Row>
+
+                        <Row>
+                            <Col xs={6}>
+                                <p className={styles.label}>Reset Highscores</p>
+                            </Col>
+                            <Col xs={6}>
+                                <Button variant="danger">Reset</Button>
+                            </Col>
+                        </Row>
+
+                        <Row>
+                            <Col xs={6}>
+                                <p className={styles.label}>Delete Account</p>
+                            </Col>
+                            <Col xs={6}>
+                                <Button variant="danger">Delete</Button>
+                            </Col>
+                        </Row>
+                    </Card.Body>
+                </Card>
+            </CardColumns>
         </Container>
     )
 }
 
 export default ProfilePage;
-
