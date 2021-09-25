854f1676400b5d9ce5407e602ced670d4e2e625d
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2021-09-25 20:41:48 +0100

    Profile | Added Reset Stats & Flash Card Buttons -> Danger Zone Card

--- src/components/user/profile/DangerZone.tsx
+++ src/components/user/profile/DangerZone.tsx
@@ -2,9 +2,9 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Button, Card, Col, Row } from "react-bootstrap";
 import { faLock, faLockOpen } from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
-import styles from "../../../styles/sass/components/user/profile/DangerZone.module.scss";
 import InfoButton from "../../ui/buttons/InfoButton";
 import PopOver from "../../ui/PopOver";
+import styles from "../../../styles/sass/components/user/profile/DangerZone.module.scss";
 
 const DangerZone = () => {
 
@@ -23,6 +23,16 @@ const DangerZone = () => {
     const resetHighScorePopover = <PopOver
         title="Reset Highscore Data"
         text="Resets all your saved scores across the app. Any ranks you currently hold on the high-scores will be lost."
+    />
+
+    const resetStats = <PopOver
+        title="Reset Stats"
+        text="Resets all your tracked statistics such as games played or won. This does not include flash card data."
+    />
+
+    const resetFlashCard = <PopOver
+        title="Reset Flash Cards"
+        text="Resets all your flash card data. Does not affect stats."
     />
 
     const deleteAccountPopover = <PopOver
@@ -58,23 +68,23 @@ const DangerZone = () => {
 
                 <div className={locked ? styles.locked : styles.unlocked}>
                     <Row className={styles.row}>
-                        <Col xs={6}>
-                            <p className={styles.label}>
-                                Clear Local Storage
-                                <InfoButton popover={localStoragePopover} className={styles.info} />
-                            </p>
+                        <Col xs={5} className={styles.col}>
+                            <p className={styles.label}>Clear Local Storage</p>
+                        </Col>
+                        <Col xs={1} className={styles.col}>
+                            <InfoButton popover={localStoragePopover} className={styles.info} />
                         </Col>
                         <Col xs={6}>
-                            <Button variant="danger" className={styles.button} disabled={locked}>Clear</Button>
+                            <Button variant="warning" className={styles.button} disabled={locked}>Clear</Button>
                         </Col>
                     </Row>
 
                     <Row className={styles.row}>
-                        <Col xs={6}>
-                            <p className={styles.label}>
-                                Reset Highscores
-                                <InfoButton popover={resetHighScorePopover} className={styles.info} />
-                            </p>
+                        <Col xs={5} className={styles.col}>
+                            <p className={styles.label}>Reset Highscores</p>
+                        </Col>
+                        <Col xs={1} className={styles.col}>
+                            <InfoButton popover={resetHighScorePopover} className={styles.info} />
                         </Col>
                         <Col xs={6}>
                             <Button variant="danger" className={styles.button} disabled={locked}>Reset</Button>
@@ -82,11 +92,35 @@ const DangerZone = () => {
                     </Row>
 
                     <Row className={styles.row}>
+                        <Col xs={5} className={styles.col}>
+                            <p className={styles.label}>Reset Stats</p>
+                        </Col>
+                        <Col xs={1} className={styles.col}>
+                            <InfoButton popover={resetStats} className={styles.info} />
+                        </Col>
                         <Col xs={6}>
-                            <p className={styles.label}>
-                                Delete Account
-                                <InfoButton popover={deleteAccountPopover} className={styles.info} />
-                            </p>
+                            <Button variant="danger" className={styles.button} disabled={locked}>Reset</Button>
+                        </Col>
+                    </Row>
+
+                    <Row className={styles.row}>
+                        <Col xs={5} className={styles.col}>
+                            <p className={styles.label}>Reset Flash Cards</p>
+                        </Col>
+                        <Col xs={1} className={styles.col}>
+                            <InfoButton popover={resetFlashCard} className={styles.info} />
+                        </Col>
+                        <Col xs={6}>
+                            <Button variant="danger" className={styles.button} disabled={locked}>Reset</Button>
+                        </Col>
+                    </Row>
+
+                    <Row className={styles.row}>
+                        <Col xs={5} className={styles.col}>
+                            <p className={styles.label}>Delete Account</p>
+                        </Col>
+                        <Col xs={1} className={styles.col}>
+                            <InfoButton popover={deleteAccountPopover} className={styles.info} />
                         </Col>
                         <Col xs={6}>
                             <Button variant="danger" className={styles.button} disabled={locked}>Delete</Button>
--- src/tests/components/user/profile/DangerZone.test.tsx
+++ src/tests/components/user/profile/DangerZone.test.tsx
@@ -6,7 +6,9 @@ const setup = () => {
     return {
         lock: component.getByTitle('Un-Lock'),
         clearLocalStorage: component.getByText('Clear'),
-        resetHighScores: component.getByText('Reset'),
+        resetHighScores: component.getAllByText('Reset')[0],
+        resetStats: component.getAllByText('Reset')[1],
+        resetFlashCards: component.getAllByText('Reset')[2],
         deleteAccount: component.getByText('Delete'),
         ...component
     }
@@ -19,14 +21,28 @@ test('Clicking the lock should enable the clear local storage button', () => {
     expect(clearLocalStorage).not.toBeDisabled();
 });
 
-test('Clicking the lock should enable the clear reset high-scores button', () => {
+test('Clicking the lock should enable the reset high-scores button', () => {
     const { lock, resetHighScores } = setup();
     expect(resetHighScores).toBeDisabled();
     fireEvent.click(lock);
     expect(resetHighScores).not.toBeDisabled();
 });
 
-test('Clicking the lock should enable the clear delete account button', () => {
+test('Clicking the lock should enable the reset stats button', () => {
+    const { lock, resetStats } = setup();
+    expect(resetStats).toBeDisabled();
+    fireEvent.click(lock);
+    expect(resetStats).not.toBeDisabled();
+});
+
+test('Clicking the lock should enable the reset flash cards button', () => {
+    const { lock, resetFlashCards } = setup();
+    expect(resetFlashCards).toBeDisabled();
+    fireEvent.click(lock);
+    expect(resetFlashCards).not.toBeDisabled();
+});
+
+test('Clicking the lock should enable the delete account button', () => {
     const { lock, deleteAccount } = setup();
     expect(deleteAccount).toBeDisabled();
     fireEvent.click(lock);
