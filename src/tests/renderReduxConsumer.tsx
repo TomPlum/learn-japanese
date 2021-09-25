bc36840ea0b4769147b0e764203968301e1f37e1
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2021-09-25 10:41:30 +0100

    Testing | Actually Committed ErrorContainer Test

--- src/tests/renderReduxConsumer.tsx
+++ src/tests/renderReduxConsumer.tsx
@@ -1,12 +1,13 @@
 import React, { ReactElement, ReactNode } from 'react';
 import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
-import { store } from '../store'
+import { store as defaultStore } from '../store'
+import { Store } from "redux";
 
 
-const renderReduxConsumer = (component: ReactElement) => {
+const renderReduxConsumer = (component: ReactElement, store?: Store, initialState?: {}) => {
     const Wrapper = (props: { children?: ReactNode }) => (
-        <Provider store={store}>
+        <Provider store={store ? store : defaultStore}>
             {props.children}
         </Provider>
     );
