5af1d3baf44530d45d525c57f4ecf1a190af27f1
WillPIum <will.plum@hotmail.com>
2021-09-26 19:13:15 +0100

    Testing | Cleaned up Cypress Tests and added new lives test. Also fixed Katakana "ji"

---
+++ cypress/integration/general.spec.ts
@@ -0,0 +1,42 @@
+import {Topic} from "../support/Topic";
+import hiragana from "../../src/data/Hiragana";
+import katakana from "../../src/data/Katakana";
+
+it.skip('Completes a full game of the Rōmaji preset', () => {
+
+    //Navigates to Joyo Kanji topic preset selection screen and starts the Rōmaji game
+    cy.navigateToMenu();
+    cy.selectTopic('Rōmaji');
+    cy.contains('Start').click();
+
+    //Sets answer to all kana data and types in the correct answer and submits answer until game is finished.
+    for (let i = 0; i < 214; i++) {
+        cy.class('QuestionDisplay_question').then((value: any) => {
+            const answer = hiragana().concat(katakana()).find(kana => kana.name == value.text())
+            if (answer) {
+                cy.get('input[placeholder=Rōmaji').type(answer.romaji[0])
+            }
+            cy.class('SubmitButton_button').click()
+        })
+    }
+
+
+
+});
+
+it('Check that lives are working correctly', () => {
+
+    //Navigates to Joyo Kanji topic preset selection screen and starts the Rōmaji game
+    cy.navigateToMenu();
+    cy.selectTopic('Rōmaji');
+    cy.contains('Start').click();
+
+    //Gets number of lives, types in an incorrect answer into the answer box and submits the wrong answer
+    let lives = cy.class('LifeDisplay_quantity')
+    cy.get('input[placeholder=Rōmaji').type('Tomiscringe')
+    cy.class('SubmitButton_button').click()
+
+    //Checks that the number of lives has gone from 5 to 4
+    cy.class('LifeDisplay_quantity').contains(4)
+
+});
--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -4,8 +4,9 @@ declare namespace Cypress {
     interface Chainable {
        class(className: string): Chainable<any>;
        getByTitle(titleName: string): Chainable<any>;
-       startGame(presetName: string, topicName?: Topic): Chainable<any>;
+       startGame(): Chainable<any>;
        startAndQuit(presetName: string, topicName?: Topic): Chainable<any>;
        navigateToMenu(): Chainable<any>;
+       selectTopic(presetName: string, topicName?: Topic): Chainable<any>;
     }
 }
