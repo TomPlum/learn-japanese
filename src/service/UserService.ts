11822519eb106367c3b925968c5d1b485afe3e3a
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2021-10-12 17:54:25 +0100

    User | Implemented Get Preferences Service Method

--- src/service/UserService.ts
+++ src/service/UserService.ts
@@ -1,12 +1,40 @@
 import RestClient, { APIResponse } from "../rest/RestClient";
 import UpdateResponse from "../rest/UpdateResponse";
+import DataResponse from "../rest/DataResponse";
+
+export interface UserPreferences {
+    defaultFont: string;
+    theme: string;
+    language: string;
+    highScores: string;
+    defaultMode: string;
+    cardsPerDay: number;
+    confidenceMenuStyle: string;
+}
 
 export default class UserService {
+    /**
+     * Updates the nickname of the current user in context.
+     * @param nickname The desired new nickname of the user.
+     * @return response true if successful, else false with the reason.
+     */
     public async setNickname(nickname: string): Promise<UpdateResponse> {
         return RestClient.put("/user/set-nickname/" + nickname.trim()).then(() => {
             return { success: true };
         }).catch((response: APIResponse<UpdateResponse>) => {
             return { success: false, error: response.error };
         });
+    }
+
+    /**
+     * Gets the application preferences of the current user in context.
+     * @return response The users preferences or an error if not found.
+     */
+    public async getPreferences(): Promise<DataResponse<UserPreferences>> {
+        return RestClient.get<UserPreferences>("/user/preferences").then(response => {
+            return { data: response.data };
+        }).catch(response => {
+            return { error: response.error };
+        })
     }
 }
--- src/tests/service/UserService.test.ts
+++ src/tests/service/UserService.test.ts
@@ -1,37 +1,77 @@
-import UserService from "../../service/UserService";
+import UserService, { UserPreferences } from "../../service/UserService";
 import RestClient from "../../rest/RestClient";
+import DataResponse from "../../rest/DataResponse";
 
-const restClient = jest.fn();
-RestClient.put = restClient;
+const restPut = jest.fn();
+const restGet = jest.fn();
+RestClient.put = restPut;
+RestClient.get = restGet;
 
 describe("User Service", () => {
     const service = new UserService();
 
-    it("Should call the rest client with the correct endpoint", () => {
-        restClient.mockResolvedValueOnce({});
-        return service.setNickname("tom").then(() => {
-            expect(restClient).toHaveBeenCalledWith("/user/set-nickname/tom");
+    describe("Set Nickname", () => {
+        it("Should call the rest client with the correct endpoint", () => {
+            restPut.mockResolvedValueOnce({});
+            return service.setNickname("tom").then(() => {
+                expect(restPut).toHaveBeenCalledWith("/user/set-nickname/tom");
+            });
+        });
+
+        it("Should call trim the nickname if it has leading or trailing spaces", () => {
+            restPut.mockResolvedValueOnce({});
+            return service.setNickname(" tom ").then(() => {
+                expect(restPut).toHaveBeenCalledWith("/user/set-nickname/tom");
+            });
+        });
+
+        it("Should return true if the API response resolves successfully", () => {
+            restPut.mockResolvedValueOnce({});
+            return service.setNickname("tom").then(response => {
+                expect(response).toStrictEqual({ success: true });
+            });
+        });
+
+        it("Should return true if the API response is rejected", () => {
+            restPut.mockRejectedValueOnce({ error: "Internal Server Error" });
+            return service.setNickname("tom").then(response => {
+                expect(response).toStrictEqual({ success: false, error: "Internal Server Error" });
+            });
         });
     });
 
-    it("Should call trim the nickname if it has leading or trailing spaces", () => {
-        restClient.mockResolvedValueOnce({});
-        return service.setNickname(" tom ").then(() => {
-            expect(restClient).toHaveBeenCalledWith("/user/set-nickname/tom");
-        });
-    });
+    describe("Get Preferences", () => {
 
-    it("Should return true if the API response resolves successfully", () => {
-        restClient.mockResolvedValueOnce({});
-        return service.setNickname("tom").then(response => {
-            expect(response).toStrictEqual({ success: true });
-        });
-    });
+        const validPreferencesResponse = {
+            defaultFont: "Gothic",
+            theme: "Dark Mode",
+            language: "English",
+            highScores: "Ask Each Time",
+            defaultMode: "Play",
+            cardsPerDay: 10,
+            confidenceMenuStyle: "Numbers 1 - 6"
+        };
 
-    it("Should return true if the API response is rejected", () => {
-        restClient.mockRejectedValueOnce({ error: "Internal Server Error" });
-        return service.setNickname("tom").then(response => {
-            expect(response).toStrictEqual({ success: false, error: "Internal Server Error" });
+        it("Should call the rest client with the correct endpoint", () => {
+            restGet.mockResolvedValueOnce({ data: validPreferencesResponse });
+            return service.getPreferences().then(() => {
+                expect(restGet).toHaveBeenCalledWith("/user/preferences");
+            });
+        });
+
+        it("Should return the preferences if the API call is successful", () => {
+            restGet.mockResolvedValueOnce({ data: validPreferencesResponse });
+            return service.getPreferences().then((response: DataResponse<UserPreferences>) => {
+                expect(response.data).toBe(validPreferencesResponse);
+            });
+        });
+
+        it("Should return undefined data with an error message if the API call fails", () => {
+            restGet.mockRejectedValueOnce({ error: "Internal Server Error" });
+            return service.getPreferences().then((response: DataResponse<UserPreferences>) => {
+                expect(response.data).toBeUndefined();
+                expect(response.error).toBe("Internal Server Error");
+            });
         });
     });
 });
