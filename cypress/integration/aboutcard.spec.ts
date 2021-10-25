e2050304d9bb5c0f406346f1347c65766158690d
WillPIum <will.plum@hotmail.com>
2021-10-25 21:07:39 +0100

    Testing | Added Nickname Change Test

--- cypress/integration/aboutcard.spec.ts
+++ cypress/integration/aboutcard.spec.ts
@@ -1,6 +1,20 @@
 it('Test you can change nickname', () => {
-    cy.login()
-    cy.contains('Test Account').click()
-    cy.contains('Profile').click()
+    cy.login().then(() => {
+        cy.request({
+            url: Cypress.env('host') + "/user/set-nickname/Test%20Account",
+            method: "PUT",
+            headers: {"Authorization": "Bearer " + JSON.parse(localStorage.getItem('user')!).token}
+        })
+
+    })
+
+    cy.visit('#/profile')
+
     cy.contains('About')
+    cy.contains('Test Account')
+
+    cy.class('About_card').class('edit').click({force: true})
+    cy.get('input[placeholder=Nickname]').clear().type('Test Account Change')
+    cy.class('About_save').class('About_icon').click({force: true})
+    cy.contains('Test Account Change')
 });
