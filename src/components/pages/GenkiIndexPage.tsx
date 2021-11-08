960e43df3203001897c2b1168f3895f91c838d84
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2021-11-08 13:48:29 +0000

    Genki | Added React App Rewired w/MJS Support & Added Table Filter + Animation

--- src/components/pages/GenkiIndexPage.tsx
+++ src/components/pages/GenkiIndexPage.tsx
@@ -1,21 +1,50 @@
 import GenkiService from "../../service/GenkiService";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import { Alert, Container } from "react-bootstrap";
 import styles from "../../styles/sass/components/pages/GenkiIndexPage.module.scss";
 import SearchField from "../ui/fields/SearchField";
 import { faCheckCircle, faCircleNotch, faExclamationCircle } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import GenkiVocabTable, { TableData } from "../ui/table/GenkiVocabTable";
+import { Row, useAsyncDebounce, useGlobalFilter, usePagination, useSortBy, useTable } from "react-table";
+import { AnimatePresence, motion } from "framer-motion";
+import Pagination from "../ui/table/Pagination";
+
+interface TableData {
+    lesson: number;
+    kana: string;
+    meaning: string;
+    kanji: string;
+}
 
 const GenkiIndexPage = () => {
 
     const service = new GenkiService();
     const data = useRef<TableData[]>([]);
 
+    const columns = useMemo(() => [
+        { Header: 'Kana', accessor: 'kana' },
+        { Header: 'Kanji', accessor: 'kanji' },
+        { Header: 'Meaning', accessor: 'meaning' },
+        { Header: 'Lesson', accessor: 'lesson' }
+    ], []);
+
+    // @ts-ignore
+    const table = useTable({ columns: columns, data: data.current }, useGlobalFilter, useSortBy, usePagination);
+
+    const spring = useMemo(() => ({ type: 'spring', damping: 50, stiffness: 100, }), []);
+
+    const {
+        // @ts-ignore
+        getTableProps, getTableBodyProps, headerGroups, prepareRow, page, canPreviousPage, canNextPage,
+        // @ts-ignore
+        pageOptions, pageCount, gotoPage, nextPage, previousPage, setPageSize, state: { pageIndex, pageSize, globalFilter },
+        // @ts-ignore
+        setGlobalFilter, preGlobalFilteredRows
+    } = table
 
     const [error, setError] = useState<string | undefined>(undefined);
     const [loading, setLoading] = useState(false);
-    const [search, setSearch] = useState("");
+    const [search, setSearch] = useState(globalFilter);
 
     useEffect(() => {
         setLoading(true);
@@ -41,9 +70,10 @@ const GenkiIndexPage = () => {
         });
     }, []);
 
-    const onSearch = (value: string) => {
-        setSearch(value.toLowerCase());
-    }
+    const onSearch = useAsyncDebounce((value: string) => {
+        setSearch(value);
+        setGlobalFilter(value);
+    }, 200);
 
     return (
         <Container fluid className={styles.wrapper}>
@@ -59,9 +89,71 @@ const GenkiIndexPage = () => {
                 </>
             }</Alert>
 
-            <SearchField onChange={onSearch} value={search} className={styles.search} disabled={loading}/>
+            <SearchField
+                value={search}
+                disabled={loading}
+                onChange={onSearch}
+                className={styles.search}
+                append={`${preGlobalFilteredRows?.length ?? 0} Results`}
+            />
 
-            {data && <GenkiVocabTable data={data.current} />}
+            {data.current && <>
+                <table {...getTableProps()} className={styles.table}>
+                    <thead>
+                    {headerGroups.map(headerGroup => (
+                        <tr {...headerGroup.getHeaderGroupProps()}>{
+                            headerGroup.headers.map(column => (
+                                <motion.th {...column.getHeaderProps({
+                                    // @ts-ignore
+                                    layoutTransition: spring,
+                                    style: { minWidth: column.minWidth }
+                                })}>
+                                    {column.render('Header')}
+                                </motion.th>
+                            ))
+                        }</tr>
+                    ))}
+                    </thead>
+
+                    <tbody {...getTableBodyProps()}>
+                    <AnimatePresence>{
+                        page.map((row: Row<any>) => {
+                            prepareRow(row);
+                            const colourClass = row.original.lesson < 12 ? styles.genkiOne : styles.genkiTwo;
+                            return (
+                                <motion.tr {...row.getRowProps({
+                                        // @ts-ignore
+                                        layoutTransition: spring,
+                                        exit: { opacity: 0, maxHeight: 0 }
+                                    })} className={colourClass}
+                                >{
+                                    row.cells.map(cell => {
+                                        return (
+                                            // @ts-ignore
+                                            <motion.td {...cell.getCellProps({ layoutTransition: spring })}>
+                                                {cell.render('Cell')}
+                                            </motion.td>
+                                        )
+                                    })
+                                }</motion.tr>
+                            )
+                        })
+                    }</AnimatePresence>
+                    </tbody>
+                </table>
+
+                <Pagination
+                    currentPage={pageIndex + 1}
+                    totalPages={pageOptions.length}
+                    canPreviousPage={canPreviousPage}
+                    canNextPage={canNextPage}
+                    onPreviousPage={previousPage}
+                    onNextPage={nextPage}
+                    onFirstPage={() => gotoPage(0)}
+                    onLastPage={() => gotoPage(pageCount - 1)}
+                    onChangeQuantity={(value: number) => setPageSize(value)}
+                />
+            </>}
         </Container>
     );
 }
