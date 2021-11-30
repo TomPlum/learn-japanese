20dfe8342d1a60b47320ed7e46ccb81c5b1c278b
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2021-11-30 17:02:22 +0000

    Cypress | Fixed Command TypeScript IntelliSense & Added Missing Semi-Colons

--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -1,12 +1,14 @@
 import {Topic} from "./Topic";
 
-declare namespace Cypress {
-    interface Chainable {
-       class(className: string): Chainable<any>;
-       getByTitle(titleName: string): Chainable<any>;
-       startGame(): Chainable<any>;
-       startAndQuit(presetName: string, topicName?: Topic): Chainable<any>;
-       navigateToMenu(): Chainable<any>;
-       selectTopic(presetName: string, topicName?: Topic): Chainable<any>;
+declare global {
+    declare namespace Cypress {
+        interface Chainable {
+            class(className: string): Chainable<any>;
+            getByTitle(titleName: string): Chainable<any>;
+            startGame(): Chainable<any>;
+            startAndQuit(presetName: string, topicName?: Topic): Chainable<any>;
+            navigateToMenu(): Chainable<any>;
+            selectTopic(presetName: string, topicName?: Topic): Chainable<any>;
+        }
     }
 }
