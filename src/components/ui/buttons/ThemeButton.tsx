7cbc992f6f8588cbc29d8b4857d577803420332c
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2021-12-06 21:50:42 +0000

    Theme | Added Theme Button Hover Colours

--- src/components/ui/buttons/ThemeButton.tsx
+++ src/components/ui/buttons/ThemeButton.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Theme } from "../../../domain/Theme";
 import { faLightbulb, faMoon } from "@fortawesome/free-solid-svg-icons";
 import menuStyles from "../../../styles/sass/components/layout/ControlsMenu.module.scss";
+import styles from "../../../styles/sass/components/ui/buttons/ThemeButton.module.scss";
 import NavigationButton from "../NavigationButton";
 
 export interface ThemeButtonProps {
@@ -24,13 +25,15 @@ const ThemeButton = (props: ThemeButtonProps) => {
         }
     }
 
+    const iconClassName = styles[`theme-${theme.toLowerCase().replaceAll(" ", "-")}`];
+
     return (
         <NavigationButton
             disableDropdown
             id="theme-button"
             onClick={handleOnClick}
             className={props.className}
-            iconClass={menuStyles.icon}
+            iconClass={[menuStyles.icon, iconClassName].join(" ")}
             textClass={menuStyles.linkText}
             icon={theme === Theme.DARK ? faLightbulb : faMoon}
         />
