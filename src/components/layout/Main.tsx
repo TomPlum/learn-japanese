6eb3ca06e3ae2aee1c7df32d8e43e87abba8c67a
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2021-12-09 14:08:10 +0000

    House Keeping | Refactored Main -> Functional Component

--- src/components/layout/Main.tsx
+++ src/components/layout/Main.tsx
@@ -1,22 +1,19 @@
-import { Component } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import LandingPage from "../pages/LandingPage";
 import styles from "../../styles/sass/components/layout/Main.module.scss";
 import NavigableMain from "./NavigableMain";
 
-class Main extends Component {
-    render() {
-        return (
-            <div className={styles.wrapper}>
-                <BrowserRouter basename={process.env.REACT_APP_BASE_PATH}>
-                    <Switch>
-                        <Route exact path="/" component={LandingPage}/>
-                        <Route component={NavigableMain} />
-                    </Switch>
-                </BrowserRouter>
-            </div>
-        );
-    }
+const Main = () => {
+    return (
+        <div className={styles.wrapper}>
+            <BrowserRouter basename={process.env.REACT_APP_BASE_PATH}>
+                <Switch>
+                    <Route exact path="/" component={LandingPage}/>
+                    <Route component={NavigableMain} />
+                </Switch>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default Main;
