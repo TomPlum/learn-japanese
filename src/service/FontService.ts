8a9421f1db2ded47914f1941be4b482bf29f36b8
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2021-12-09 13:46:29 +0000

    Fonts | Font Service - Exposed "Get Fonts" Function & Added TS Doc

--- src/service/FontService.ts
+++ src/service/FontService.ts
@@ -5,6 +5,26 @@ class FontService {
 
     private readonly _repository = new FontRepository();
 
+    /**
+     * Retrieves all available fonts.
+     * These fonts are specifically for Japanese kanji and/or kana.
+     * @return fonts An array of fonts.
+     */
+    public getFonts(): Promise<Font[]> {
+        return this._repository.read().then(response => {
+            return response;
+        });
+    }
+
+    /**
+     * Retrieves the currently selected global kanji font.
+     *
+     * Attempts to retrieve it from the browsers local storage.
+     * The font is cached by the Redux reducer as is dispatched by
+     * the font selector on the navigation bar.
+     *
+     * @return font The selected font, or else undefined if not set.
+     */
     public getSelectedFont(): Promise<Font | undefined> {
         return this._repository.read().then(response => {
             const cachedSelection = localStorage.getItem("font") ?? "";
--- src/tests/service/FontService.test.ts
+++ src/tests/service/FontService.test.ts
@@ -6,18 +6,33 @@ jest.mock("../../repository/FontRepository", () => {
     return function () { return { read: mockFontRepository } }
 });
 
+const fonts = [
+    { displayName: "Default", name: "" },
+    { displayName: "Handwriting", name: "SanafonMugi Handwriting" },
+    { displayName: "Gothic", name: "K Gothic" },
+    { displayName: "Mincho", name: "Appli Mincho" }
+];
+
 describe("Font Service", () => {
     const service = new FontService();
 
     beforeEach(() => {
-        mockFontRepository.mockResolvedValueOnce([
-            { displayName: "Default", name: "" },
-            { displayName: "Handwriting", name: "SanafonMugi Handwriting" },
-            { displayName: "Gothic", name: "K Gothic" },
-            { displayName: "Mincho", name: "Appli Mincho" }
-        ]);
+        mockFontRepository.mockResolvedValueOnce(fonts);
+        localStorageMock.clear();
+    });
 
-       localStorageMock.clear();
+    describe("Get Fonts", () => {
+        it("Should call the repository", () => {
+            return service.getFonts().then(() => {
+                expect(mockFontRepository).toHaveBeenCalled();
+            });
+        });
+
+        it("Should return the fonts from the repository", () => {
+            return service.getFonts().then(fonts => {
+                expect(fonts).toStrictEqual(fonts);
+            });
+        });
     });
 
     describe("Get Selected Font", () => {
