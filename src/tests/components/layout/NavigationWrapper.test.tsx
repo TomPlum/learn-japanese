4d30bd851f3365820309761be5187cb6192d2761
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2021-12-27 21:13:00 +0000

    Testing | Fixed NavigationWrapper Test File Name & Fixed Async Warning

--- src/tests/components/layout/NavigationWrapper.test.tsx
+++ src/tests/components/layout/NavigationWrapper.test.tsx
@@ -16,12 +16,12 @@ test('Clicking the login button while not logged in should launch the user modal
     expect(await screen.findByTestId('user-modal')).toBeInTheDocument();
 });
 
-test('Clicking "x" close button in the user modal should close it', () => {
+test('Clicking "x" close button in the user modal should close it', async () => {
     const { login } = setup();
 
     //Clicking login should load the modal
     fireEvent.click(login);
-    const userModal = screen.getByTestId('user-modal');
+    const userModal = await screen.findByTestId('user-modal');
     expect(userModal).toBeInTheDocument();
 
     //Clicking the 'x' button should close it
