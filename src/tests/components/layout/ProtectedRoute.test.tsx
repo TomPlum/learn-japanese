338becc1161f5948450520c1ca3008a3a10f8f54
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2021-12-28 09:50:57 +0000

    Routing | Implemented Protected Route Component

---
+++ src/tests/components/layout/ProtectedRoute.test.tsx
@@ -0,0 +1,47 @@
+import { render, screen } from "@testing-library/react";
+import ProtectedRoute, { ProtectedRouteProps } from "../../../components/layout/ProtectedRoute";
+import { createMemoryHistory } from "history";
+import { Router, Switch } from "react-router-dom";
+
+let props: ProtectedRouteProps;
+const history = createMemoryHistory();
+const mockRender = jest.fn();
+
+beforeEach(() => {
+    props = {
+        isAuthenticated: true
+    };
+});
+
+const setup = () => {
+    const component = render(<Router history={history}><Switch><ProtectedRoute {...props} /></Switch></Router>);
+    return {
+        ...component
+    }
+}
+
+test('Should redirect to the main page if the user is not authenticated', () => {
+    props.isAuthenticated = false;
+    try {
+        setup();
+    } catch (e) {
+        // TODO: Why does it infinitely loop? Causes React render stack overflow.
+    }
+    expect(history.location.pathname).toBe("/menu/learn")
+});
+
+test('Should render the given component if the user is authenticated', () => {
+    props.component = TestComponent;
+    setup();
+    expect(screen.getByText('I\'m a test component')).toBeInTheDocument();
+});
+
+test('Should call the given render function if the user is authenticated and no component is passed', () => {
+    props.render = mockRender;
+    setup();
+    expect(mockRender).toHaveBeenCalled();
+});
+
+const TestComponent = () => {
+    return (<p>I'm a test component</p>);
+}
