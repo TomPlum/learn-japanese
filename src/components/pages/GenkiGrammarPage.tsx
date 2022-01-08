53d0d744c2c98fead9b3973a81d9bd74f5a59ba0
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2022-01-08 15:43:35 +0000

    Learn | Genki Grammar Page - Added Remaining Chapter 3 Grammar Info

--- src/components/pages/GenkiGrammarPage.tsx
+++ src/components/pages/GenkiGrammarPage.tsx
@@ -12,6 +12,7 @@ import ValueSelector from "../ui/select/ValueSelector";
 import Arrays from "../../utility/Arrays";
 import QuoteDisplay from "../ui/display/QuoteDisplay";
 import { useDebouncedEffect } from "../../hooks";
+import GenkiHighlightDisplay from "../ui/display/GenkiHighlightDisplay";
 
 const GenkiGrammarPage = () => {
 
@@ -418,10 +419,223 @@ const GenkiGrammarPage = () => {
         )
     }
 
+    const c3p3: GrammarInfoProps = {
+        chapter: 3,
+        section: 3,
+        page: PageNumber.of(88, 89),
+        title: "Particles",
+        body: (
+            <div>
+                <p>Nouns used in sentences generally must be followed by particles, which indicate the relations that
+                    the nouns bear to the verbs. This section contains four particles: で, に, へ, and を.
+                </p>
+
+                <GenkiHighlightDisplay
+                    text="で"
+                    chapter={3}
+                    description="The particle で indicates where the even described by the verb takes place."
+                />
+                <GenkiExampleTable
+                    values={[{
+                        japanese: { value: "うちでテレビをみます。", underline: "で" },
+                        english: { value: "I will watch TV at home." }
+                    }]}
+                />
+
+                <GenkiHighlightDisplay
+                    text="に"
+                    chapter={3}
+                    description="The particle に indicates two things; the goal toward which things move; and the time at which an event takes place."
+                />
+                <GenkiExampleTable
+                    values={[
+                        {
+                            japanese: { value: "わたしはうちにかえります。", underline: "に" },
+                            english: { value: "I will return." }
+                        },
+                        {
+                            japanese: { value: "じゅいちじにねます。", underline: "に" },
+                            english: { value: "I will go to bed at eleven." }
+                        }
+                    ]}
+                />
+
+                <GenkiHighlightDisplay
+                    text="へ"
+                    chapter={3}
+                    description="The particle へ also indicates the goal of a movement. It is pronounced “e.”"
+                />
+                <GenkiExampleTable
+                    values={[{
+                        japanese: { value: "わたしはうちへかえります。", underline: "へ" },
+                        english: { value: "I will return." }
+                    }]}
+                />
+
+                <GenkiHighlightDisplay
+                    text="を"
+                    chapter={3}
+                    description="The particle を indicates direct objects. It is pronounced “o.”"
+                />
+                <GenkiExampleTable
+                    values={[
+                        {
+                            japanese: { value: "コーヒーをのみます。", underline: "を" },
+                            english: { value: "I drink coffee." }
+                        },
+                        {
+                            japanese: { value: "テープをききます。 ", underline: "を" },
+                            english: { value: "I listen to tapes." }
+                        }
+                    ]}
+                />
+            </div>
+        )
+    }
+
+    const c3p4: GrammarInfoProps = {
+        chapter: 3,
+        section: 4,
+        page: PageNumber.from(90),
+        title: "Time References",
+        body: (
+            <div>
+                <p>You need the particle に with the days of the week, numerical time expressions, and months.
+                    You do not need to use the time particle に with time expressions defined relative to the present
+                    moment, such as “today” and “tomorrow”, expressions describing regular intervals such as “every day”,
+                    or the word for “when.”
+                </p>
+
+                <GenkiExampleTable
+                    values={[
+                        {
+                            japanese: { value: "にちようびにいきます。", underline: "にちようびに"　},
+                            english: { value: "I will go on Sunday." }
+                        },
+                        {
+                            japanese: { value: "くがつにかえります。", underline: "くがつに" },
+                            english: { value: "I will go back in September" }
+                        }
+                    ]}
+                />
+            </div>
+        )
+    }
+
+    const c3p5: GrammarInfoProps = {
+        chapter: 3,
+        section: 5,
+        page: PageNumber.from(90),
+        title: "～ませんか",
+        body: (
+            <div>
+                <p>～ませんか can be used to extend an invitation. It should be noted that the affirmative counterpart
+                    of this (ますか) cannot be so used.
+                </p>
+
+                <GenkiExampleTable
+                    values={[
+                        {
+                            japanese: { value: "テニスをしませんか。"　},
+                            english: { value: "Will you play tennis with me?" }
+                        },
+                        {
+                            japanese: { value: "ひるごはんをたべませんか。" },
+                            english: { value: "What do you say to having lunch with me?" }
+                        }
+                    ]}
+                />
+            </div>
+        )
+    }
+
+    const c3p6: GrammarInfoProps = {
+        chapter: 3,
+        section: 6,
+        page: PageNumber.from(91),
+        title: "Frequency Adverbs",
+        body: (
+            <div>
+                <p>Frequency adverbs are added to indicate how often something is done.</p>
+                <p>Examples of frequency adverbs are まいにち (everyday), よく (often), and ときどき (sometimes).</p>
+
+                <GenkiExampleTable
+                    values={[{
+                        japanese: { value: "わたしはときどききっさてんにいきます。"　},
+                        english: { value: "I sometimes go to a coffee shop." }
+                    }]}
+                />
+
+                <p>It is also important to remember that adverbs of infrequency must be followed by a negative at the end of the sentence.</p>
+
+                <GenkiExampleTable
+                    values={[{
+                        japanese: { value: "わたしはぜんぜんテレビをみません。"　},
+                        english: { value: "I do not watch TV at all." }
+                    }]}
+                />
+            </div>
+        )
+    }
+
+    const c3p7: GrammarInfoProps = {
+        chapter: 3,
+        section: 7,
+        page: PageNumber.from(91),
+        title: "Word Order",
+        body: (
+            <div>
+                <p>Japanese sentences are structured as Subject-Object-Verb.</p>
+
+                <p>The following example has the structure
+                    <QuoteDisplay chapter={3}>{"topic -> time -> place -> object -> verb"}</QuoteDisplay>
+                </p>
+                <GenkiExampleTable
+                    values={[{
+                        japanese: { value: "わたしはきょうとしょかんでにほんごをべんきょうします。" },
+                        english: { value: "I will study Japanese in the library today." }
+                    }]}
+                />
+
+                <p>This example has the structure
+                    <QuoteDisplay chapter={3}>{"topic -> frequency -> time -> goal -> verb"}</QuoteDisplay>
+                </p>
+                <GenkiExampleTable
+                    values={[{
+                        japanese: { value: "わたしはよくしちじごろうちへかえります。" },
+                        english: { value: "I often go back at around seven." }
+                    }]}
+                />
+            </div>
+        )
+    }
+
+    const c3p8: GrammarInfoProps = {
+        chapter: 3,
+        section: 8,
+        page: PageNumber.of(91, 92),
+        title: "The Topic Particle は",
+        body: (
+            <div>
+                <p>The particle は presents the topic of one’s utterance (“As for item X, it is such that…”).
+                    It puts forward the item that you want to talk about and comment on.
+                    A topic phrase, however, need not be the subject of a sentence.
+                </p>
+
+               <GenkiExampleTable
+                   values={[{
+                       japanese: { value: "アリーさん、しゅまつはたいていなにをしますか。" },
+                       english: { value: "(Mary, what do you usually do on the weekends?)" }
+                   }]}
+               />
+            </div>
+        )
+    }
+
     const allGrammar: GrammarInfoProps[] = [
         c1p1, c1p2, c1p3,
         c2p1, c2p2, c2p3, c2p4, c2p5, c2p6, c2p7,
-        c3p1, c3p2
+        c3p1, c3p2, c3p3, c3p4, c3p5, c3p6, c3p7, c3p8
     ];
 
     const [grammar, setGrammar] = useState(allGrammar);
