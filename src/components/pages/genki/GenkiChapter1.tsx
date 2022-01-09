e868905518cc6b842461e494942910b5d373574e
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2022-01-09 09:56:32 +0000

    Learn | Extracted GenkiGrammarPage Chapter Specific Components

---
+++ src/components/pages/genki/GenkiChapter1.tsx
@@ -0,0 +1,73 @@
+import { GrammarInfoProps } from "../../learn/GrammarInfo";
+import PageNumber from "../../../domain/learn/PageNumber";
+import QuoteDisplay from "../../ui/display/QuoteDisplay";
+import GenkiExampleTable from "../../ui/display/GenkiExampleTable";
+import styles from "../../../styles/sass/components/pages/GenkiGrammarPage.module.scss";
+import React from "react";
+
+const GenkiChapter1 = () => {
+    const c1p1: GrammarInfoProps = {
+        chapter: 1,
+        section: 1,
+        title: "X は Y です",
+        page: PageNumber.of(41, 42),
+        body: (
+            <div>
+                <p>
+                    <QuoteDisplay chapter={1}>It is 12:30.</QuoteDisplay>
+                    <span> and </span>
+                    <QuoteDisplay chapter={1}>I am a student.</QuoteDisplay>
+                    <span>are sentences that can be translated into Japanese
+                    using an appropriate noun and the word <em>desu</em>.</span>
+                </p>
+                <GenkiExampleTable
+                    book={1}
+                    className={styles.exampleTable}
+                    values={[
+                        { japanese: { value: "じゅうにじはんです。" }, english: { value: "(It) is half past twelve" } },
+                        { japanese: { value: "がくせいです。" }, english: { value: "(I) am a student." } }
+                    ]}
+                />
+            </div>
+        )
+    }
+
+    const c1p2: GrammarInfoProps = {
+        chapter: 1,
+        section: 2,
+        title: "Question Sentences",
+        page: PageNumber.of(42, 43),
+        body: (
+            <div>
+                <p>It is most common to use the end particle ka when making a question sentence.</p>
+                <p>Simply add か to the end of a statement.</p>
+            </div>
+        )
+    }
+
+    const c1p3: GrammarInfoProps = {
+        chapter: 1,
+        section: 3,
+        title: "Noun の Noun",
+        page: PageNumber.from(43),
+        body: (
+            <div>
+                <p>No (の) is a particle that connects two nouns.</p>
+                <GenkiExampleTable
+                    book={1}
+                    className={styles.exampleTable}
+                    values={[
+                        { japanese: { value: "たけしさんのでんわばんごう。" }, english: { value: "Takeshi’s phone number" } },
+                        { japanese: { value: "だいがくのせんせい。" }, english: { value: "a college professor" } },
+                        { japanese: { value: "にほんごのがくせい。" }, english: { value: "a student of the Japanese language" } },
+                        { japanese: { value: "にほんのだいがく。" }, english: { value: "a college in Japan" } }
+                    ]}
+                />
+            </div>
+        )
+    }
+
+    return { c1p1, c1p2, c1p3 };
+}
+
+export default GenkiChapter1;
---
+++ src/components/pages/genki/GenkiChapter13.tsx
@@ -0,0 +1,154 @@
+import { GrammarInfoProps } from "../../learn/GrammarInfo";
+import PageNumber from "../../../domain/learn/PageNumber";
+import GenkiTable from "../../ui/table/GenkiTable";
+import styles from "../../../styles/sass/components/pages/GenkiGrammarPage.module.scss";
+import GenkiExampleTable from "../../ui/display/GenkiExampleTable";
+import React from "react";
+
+const GenkiChapter13 = () => {
+    const c13p1: GrammarInfoProps = {
+        chapter: 13,
+        section: 1,
+        page: PageNumber.of(26, 28),
+        title: "Potential Verbs",
+        body: (
+            <div>
+                <p>Potential verbs are used to say that someone “can” or “has the ability to” do something, or that something is possible.</p>
+
+                <p>る Verbs: Drop the final –ru and add –rareru</p>
+                <GenkiTable chapter={13} maxWidth={165}>
+                    <td>{}</td>
+                    <td>見る &#8594; 見られる</td>
+                </GenkiTable>
+
+                <p>う Verbs: Drop the final –u and add –eru</p>
+                <GenkiTable chapter={13} maxWidth={165}>
+                    <td>{}</td>
+                    <td>行く &#8594; 行ける</td>
+                </GenkiTable>
+
+                <p>Irregular Verbs</p>
+                <GenkiTable chapter={13} maxWidth={300}>
+                    <tr>
+                        <td>{}</td>
+                        <td>くる &#8594; こられる</td>
+                        <td>する &#8594; できる</td>
+                    </tr>
+                </GenkiTable>
+
+                <GenkiTable chapter={13}>
+                    <tr>
+                        <th rowSpan={2} className={styles.genkiTwo}>Verb Conjugation</th>
+                        <th colSpan={2} scope="colgroup">Short Form</th>
+                        <th colSpan={2} scope="colgroup">Long Form</th>
+                    </tr>
+                    <tr>
+                        <th scope="col">Affirmative</th>
+                        <th scope="col">Negative</th>
+                        <th scope="col">Affirmative</th>
+                        <th scope="col">Negative</th>
+                    </tr>
+                    <tr>
+                        <th scope="row">Present Tense</th>
+                        <td>見られる</td>
+                        <td>見られない</td>
+                        <td>見られます</td>
+                        <td>見られません</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Past Tense</th>
+                        <td>見られた</td>
+                        <td>見られなかった</td>
+                        <td>見られました</td>
+                        <td>見られませんでした</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">て Form</th>
+                        <td>見られて</td>
+                        <td>-</td>
+                        <td>-</td>
+                        <td>-</td>
+                    </tr>
+                </GenkiTable>
+            </div>
+        )
+    }
+
+    const c13p2: GrammarInfoProps = {
+        chapter: 13,
+        section: 2,
+        page: PageNumber.of(28, 29),
+        title: "~し",
+        body: (
+            <div>
+                <p>When you want to mention not just one, but two or more reasons, you can use し in place of から.
+                    し usually follows a predicate in the short form.
+                </p>
+
+                <GenkiTable chapter={13} maxWidth={320}>
+                    <td><span className={styles.genkiTwo}>(reason<sub>1</sub>) </span> し、
+                        <span className={styles.genkiTwo}>(reason<sub>2</sub>) </span>し、
+                        <span className={styles.genkiTwo}>(situation)</span> 。
+                    </td>
+                </GenkiTable>
+
+                <p>For example:</p>
+
+                <GenkiExampleTable
+                    book={2}
+                    values={[{
+                        japanese: { value: "家族はら手紙が来たし、かれと電話で話したし、きのうはとてもいい日でした。", underline: "し", hideRomaji: true },
+                        english: { value: "Yesterday was a great day–a letter came from my family, and I talked with my boyfriend on the phone." }
+                    }]}
+                />
+
+                <p>Note that し follows the short forms. In present tense sentences, this means that だ appears with
+                    な-adjectives and nouns, but not with い-adjectives.
+                </p>
+            </div>
+        )
+    }
+
+    const c13p3: GrammarInfoProps = {
+        chapter: 13,
+        section: 3,
+        page: PageNumber.of(29, 30),
+        title: "そうです",
+        body: (
+            <div>
+                <p>When we say …そうです, we are guessing what something is like on the basis of our impressions.</p>
+
+                <GenkiExampleTable
+                    book={2}
+                    values={[{
+                        japanese: { value: "このりんごはおいしそうです。", underline: "そうです" },
+                        english: { value: "This apple looks delicious.", underline: "looks" }
+                    }]}
+                />
+
+                <p>To form …そうです sentences with い-adjectives, drop the final い (exception - いい, change to よさ before そう);
+                    with な-adjectives, you just drop な.
+                </p>
+
+                <GenkiTable chapter={13} maxWidth={400}>
+                    <tr>
+                        <td>い Adjectives</td>
+                        <td>おいしい &#8594; おいしそうです</td>
+                    </tr>
+                    <tr>
+                        <td>(exception)</td>
+                        <td>いい &#8594; よさそうです</td>
+                    </tr>
+                    <tr>
+                        <td>な Adjectives</td>
+                        <td>元気(な) &#8594; 元気そうです</td>
+                    </tr>
+                </GenkiTable>
+            </div>
+        )
+    }
+
+    return { c13p1, c13p2, c13p3 };
+}
+
+export default GenkiChapter13;
