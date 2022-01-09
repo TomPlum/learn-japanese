5e7b993a3893b50a2edd098ebb6b73116465198e
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2022-01-09 14:43:18 +0000

    UI | Refactored Underline Component (Introduced Underlining Strategy)

--- src/components/ui/Underline.tsx
+++ src/components/ui/Underline.tsx
@@ -1,31 +1,103 @@
 import { PropsWithChildren } from "react";
 import ComponentTree from "../../utility/ComponentTree";
+import Arrays from "../../utility/Arrays";
+
+export interface UnderlineStrategy {
+    underline: (text: string, underlineClass?: string) => any;
+}
+
+/**
+ * Underlines the first match of the given string value.
+ */
+export class FirstMatch implements UnderlineStrategy {
+    private readonly _underline: string;
+
+    constructor(underline: string) {
+        this._underline = underline;
+    }
+
+    underline(text: string, underlineClass?: string) {
+        const underlineStartIndex = text.indexOf(this._underline);
+        const start = text.substring(0, underlineStartIndex);
+
+        const underlineEndIndex = underlineStartIndex + this._underline.length;
+        const underlined = text.substring(underlineStartIndex, underlineEndIndex);
+        const remaining = text.substring(underlineEndIndex);
+
+        return (
+            <span>
+                {start}
+                <span className={underlineClass}>{underlined}</span>
+                {remaining}
+            </span>
+        );
+    }
+}
+
+/**
+ * Underlines only the given occurrences of the given string value.
+ */
+export class Occurrences implements UnderlineStrategy {
+    private readonly _underline: string;
+    private readonly _occurrences: number[];
+
+    constructor(underline: string, occurrences: number[]) {
+        this._underline = underline;
+        this._occurrences = occurrences;
+    }
+
+    underline(text: string, underlineClass?: string) {
+        const occurrenceIndices = this.getOccurrenceIndices(text);
+        const nextOccurrenceIndex = Arrays.copy(occurrenceIndices).reverse();
+        const response = [];
+
+        let i = 0;
+
+        while (i < text.length - 1) {
+            const currentIndexIsUnderlineStart = occurrenceIndices.includes(i);
+            const currentOccurrenceIsWanted = this._occurrences.includes(occurrenceIndices.indexOf(i) + 1);
+
+            if (currentIndexIsUnderlineStart && currentOccurrenceIsWanted) {
+                const underlineLength = this._underline.length;
+                const underlinedText = text.substring(i, i + underlineLength);
+                response.push(
+                    <span className={underlineClass} key={underlinedText + i}>
+                        {underlinedText}
+                    </span>
+                );
+                i += underlineLength;
+            } else {
+                const occurrenceIndex = nextOccurrenceIndex.pop();
+                const nonUnderlinedText = text.substring(i, occurrenceIndex);
+                response.push(<span key={nonUnderlinedText + i}>{nonUnderlinedText}</span>)
+                i = occurrenceIndex ?? text.length;
+            }
+        }
+
+        return response;
+    }
+
+    private getOccurrenceIndices(text: string): number[] {
+        let index = text.indexOf(this._underline);
+        let indices = [];
+        while (index >= 0) {
+            indices.push(index);
+            index = text.indexOf(this._underline, index + 1);
+        }
+        return indices;
+    }
+
+}
 
 export interface UnderlineProps {
-    underline: string;
+    strategy?: UnderlineStrategy;
     underlineClass?: string;
 }
 
 const Underline = (props: PropsWithChildren<UnderlineProps>) => {
-
-    const { underline, underlineClass, children } = props;
-
+    const { strategy, underlineClass, children } = props;
     const value: string = new ComponentTree(children).getDeepestLeafNode();
-
-    const underlineStartIndex = value.indexOf(underline);
-    const start = value.substring(0, underlineStartIndex);
-
-    const underlineEndIndex = underlineStartIndex + underline.length;
-    const underlined = value.substring(underlineStartIndex, underlineEndIndex);
-    const remaining = value.substring(underlineEndIndex);
-
-    return (
-        <span>
-            {start}
-            <span className={underlineClass}>{underlined}</span>
-            {remaining}
-        </span>
-    );
+    return !!strategy ? strategy.underline(value, underlineClass) : value;
 }
 
 export default Underline;
--- src/tests/components/ui/Underline.test.tsx
+++ src/tests/components/ui/Underline.test.tsx
@@ -1,23 +1,48 @@
 import { render } from "@testing-library/react";
-import Underline from "../../../components/ui/Underline";
-import { getByTextWithElements } from "../../Queries";
+import Underline, { FirstMatch, Occurrences } from "../../../components/ui/Underline";
+import { getByTextWithElements, getByTextWithMarkup } from "../../Queries";
 
-test("Should render the whole text content from the immediate child component", () => {
-    render(
-        <Underline underline="test">
-            <span>This is test text.</span>
-        </Underline>
-    );
-    const text = getByTextWithElements('This is test text.');
-    expect(text).toBeInTheDocument();
+describe("First Match Strategy", () => {
+    test("Should render the whole text content from the immediate child component", () => {
+        render(
+            <Underline strategy={new FirstMatch("test")}>
+                <span>This is test text.</span>
+            </Underline>
+        );
+        const text = getByTextWithElements('This is test text.');
+        expect(text).toBeInTheDocument();
+    });
+
+    test("Should apply the given class to the underlined text", () => {
+        const component = render(
+            <Underline strategy={new FirstMatch("test")} underlineClass="myClass">
+                <span>This is test text.</span>
+            </Underline>
+        );
+        const underlined = component.getByText('test');
+        expect(underlined).toHaveClass('myClass');
+    });
 });
 
-test("Should apply the given class to the underlined text", () => {
-    const component = render(
-        <Underline underline="test" underlineClass="myClass">
-            <span>This is test text.</span>
-        </Underline>
-    );
-    const underlined = component.getByText('test');
-    expect(underlined).toHaveClass('myClass');
+describe("Occurrences Strategy", () => {
+    it("Should render the whole text", () => {
+        render(
+            <Underline strategy={new Occurrences("し", [1, 3])} underlineClass="myClass">
+                <span>家族はら手紙が来たし、かれと電話で話したし、きのうはとてもいい日でした。</span>
+            </Underline>
+        );
+        const text = getByTextWithMarkup('家族はら手紙が来たし、かれと電話で話したし、きのうはとてもいい日でした。');
+        expect(text).toBeInTheDocument();
+    });
+
+    it("Should highlight only the specified occurrences", () => {
+        const component = render(
+            <Underline strategy={new Occurrences("し", [1, 3])} underlineClass="myClass">
+                <span>家族はら手紙が来たし、かれと電話で話したし、きのうはとてもいい日でした。</span>
+            </Underline>
+        );
+        const underlineCharacterMatches = component.getAllByText('し');
+        expect(underlineCharacterMatches[0]).toHaveClass('myClass');
+        expect(underlineCharacterMatches[1]).toHaveClass('myClass');
+    });
 });
