0245d628920b59f3207ead2b564d1636853fd09b
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2022-01-09 10:51:55 +0000

    House Keeping | Integrated GenkiUnderlineDisplay w/GenkiExampleTable & Reduced Redundancy

--- src/components/ui/display/GenkiExampleTable.tsx
+++ src/components/ui/display/GenkiExampleTable.tsx
@@ -1,6 +1,7 @@
 import { Table } from "react-bootstrap";
 import RomajiGenerator from "../../../utility/RomajiGenerator";
 import styles from "../../../styles/sass/components/ui/display/GenkiExampleTable.module.scss";
+import GenkiUnderlineDisplay from "./GenkiUnderlineDisplay";
 
 interface ExampleValue {
     value: string;
@@ -20,23 +21,6 @@ const GenkiExampleTable = (props: GenkiExampleTableProps) => {
 
     const romajiGenerator = new RomajiGenerator();
 
-    const underlineValue = (example: ExampleValue) => {
-        const underlineText = example.underline;
-        if (underlineText) {
-            const value = example.value;
-            const underlineStartIndex = value.indexOf(underlineText);
-            const start = value.substring(0, underlineStartIndex);
-            const underlineEndIndex = underlineStartIndex + underlineText.length;
-            const underlined = value.substring(underlineStartIndex, underlineEndIndex);
-            const remaining = value.substring(underlineEndIndex);
-
-            const underlineClass = book == 1 ? styles.genkiOneUnderline : styles.genkiTwoUnderline;
-            return <span>{start}<span className={underlineClass}>{underlined}</span>{remaining}</span>
-        }
-
-        return <span>{example.value}</span>
-    }
-
     return (
         <Table className={[styles.table, className].join(" ")} borderless size="sm">
             {values.map(example => {
@@ -46,12 +30,18 @@ const GenkiExampleTable = (props: GenkiExampleTableProps) => {
                     <tbody key={`${english}-tbody`}>
                         <tr>
                             <td>
-                                <p className={styles.jp}>{underlineValue(japanese)}</p>
+                                <p className={styles.jp}>
+                                    <GenkiUnderlineDisplay underline={japanese.underline ?? ""} book={book}>
+                                        <span>{japanese.value}</span>
+                                    </GenkiUnderlineDisplay>
+                                </p>
                                 {!japanese.hideRomaji && <span>{romajiGenerator.generate(japanese.value)}</span>}
                             </td>
 
                             <td className={styles.en}>
-                                {underlineValue(english)}
+                                <GenkiUnderlineDisplay underline={english.underline ?? ""} book={book}>
+                                    <span>{english.value}</span>
+                                </GenkiUnderlineDisplay>
                             </td>
                         </tr>
                     </tbody>
