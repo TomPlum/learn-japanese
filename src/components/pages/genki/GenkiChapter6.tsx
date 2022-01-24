e3b8faca1d3b8b72fc9198f5ca488d8918753713
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2022-01-24 21:15:05 +0000

    Learn | GenkiGrammarPage - Started Chapter 6 Section 1 Table Progression

--- src/components/pages/genki/GenkiChapter6.tsx
+++ src/components/pages/genki/GenkiChapter6.tsx
@@ -2,6 +2,7 @@ import { GrammarInfoProps } from "../../learn/GrammarInfo";
 import PageNumber from "../../../domain/learn/PageNumber";
 import React from "react";
 import GenkiTable from "../../ui/table/GenkiTable";
+import styles from "../../../styles/sass/components/pages/GenkiGrammarPage.module.scss";
 
 const GenkiChapter6 = () => {
     const c6p1: GrammarInfoProps = {
@@ -11,7 +12,7 @@ const GenkiChapter6 = () => {
         title: "Te-form",
         body: (
             <div>
-                <p>The てform has many uses in Japanese. Among these uses are:</p>
+                <p>The て form has many uses in Japanese. Among these uses are:</p>
                 <ul>
                     <li>making requests (“…please.”)</li>
                     <li>giving and asking for permission (“you may…/May I…”)</li>
@@ -23,9 +24,25 @@ const GenkiChapter6 = () => {
 
                 <GenkiTable chapter={6}>
                     <tbody>
-                        <tr>
+                        <tr className={styles.rowSection}>
                             <td>Ru-verbs</td>
-                            <td></td>
+                            <td>{"る "}<span className={styles.arrow}>&#8594;</span>{" て"}</td>
+                            <td>{"食べる "}<span className={styles.arrow}>&#8594;</span>{" 食べて"}</td>
+                        </tr>
+                        <tr>
+                            <td>U-verbs with final</td>
+                            <td rowSpan={3}>
+
+                            </td>
+                            <td>{"合う "}<span className={styles.arrow}>&#8594;</span>{" 合って"}</td>
+                        </tr>
+                        <tr>
+                            <td>{"待つ "}<span className={styles.arrow}>&#8594;</span>{" 待って"}</td>
+
+                        </tr>
+                        <tr>
+                            <td>{"とる "}<span className={styles.arrow}>&#8594;</span>{" とって"}</td>
+
                         </tr>
                     </tbody>
                 </GenkiTable>
