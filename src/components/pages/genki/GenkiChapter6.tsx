b3ceb069b86fa555940a37d8ee7a3c4d7c6f1bc9
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2022-01-25 21:18:55 +0000

    Learn | GenkiGrammarPage - Started Chapter 6 Section 1 Table Progression

--- src/components/pages/genki/GenkiChapter6.tsx
+++ src/components/pages/genki/GenkiChapter6.tsx
@@ -3,6 +3,7 @@ import PageNumber from "../../../domain/learn/PageNumber";
 import React from "react";
 import GenkiTable from "../../ui/table/GenkiTable";
 import styles from "../../../styles/sass/components/pages/GenkiGrammarPage.module.scss";
+import GenkiComparisonDisplay from "../../ui/display/GenkiComparisonDisplay";
 
 const GenkiChapter6 = () => {
     const c6p1: GrammarInfoProps = {
@@ -30,19 +31,37 @@ const GenkiChapter6 = () => {
                             <td>{"食べる "}<span className={styles.arrow}>&#8594;</span>{" 食べて"}</td>
                         </tr>
                         <tr>
-                            <td>U-verbs with final</td>
-                            <td rowSpan={3}>
-
+                            <td rowSpan={10}>U-verbs with final</td>
+                            <td rowSpan={4}>
+                                <GenkiComparisonDisplay
+                                    ignoreFirstBrace
+                                    firstComparison={{ text: "うつる" }}
+                                    secondComparison={{ text: " " }}
+                                    post="って"
+                                    book={1}
+                                />
                             </td>
+                        </tr>
+                        <tr>
                             <td>{"合う "}<span className={styles.arrow}>&#8594;</span>{" 合って"}</td>
                         </tr>
                         <tr>
                             <td>{"待つ "}<span className={styles.arrow}>&#8594;</span>{" 待って"}</td>
-
                         </tr>
                         <tr>
                             <td>{"とる "}<span className={styles.arrow}>&#8594;</span>{" とって"}</td>
-
+                        </tr>
+                        <tr>
+                            <td rowSpan={4}>{"む"}</td>
+                        </tr>
+                        <tr>
+                            <td>{"読む　"}<span className={styles.arrow}>&#8594;</span>{" 読んで"}</td>
+                        </tr>
+                        <tr>
+                            <td>{"遊ぶ　"}<span className={styles.arrow}>&#8594;</span>{" 遊んで"}</td>
+                        </tr>
+                        <tr>
+                            <td>{"死ぬ　"}<span className={styles.arrow}>&#8594;</span>{" 死んで"}</td>
                         </tr>
                     </tbody>
                 </GenkiTable>
