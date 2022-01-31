7fcbf7611da74aecf15c8baca29c017c6cc4c3b5
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2022-01-31 20:54:01 +0000

    JPUI-3 | PlayWizardFooter - Exposed Intermediate & Terminal Step Properties

--- src/components/layout/wizard/play/PlayWizardFooter.tsx
+++ src/components/layout/wizard/play/PlayWizardFooter.tsx
@@ -2,14 +2,25 @@ import { Button } from "react-bootstrap";
 import styles from "../../../../styles/sass/components/layout/wizard/play/PlayWizardFooter.module.scss";
 
 export interface PlayWizardFooterProps {
+    intermediate?: boolean;
+    terminal?: boolean;
     onNext: () => void;
+    onBack?: () => void;
 }
 
 const PlayWizardFooter = (props: PlayWizardFooterProps) => {
+
+    const { intermediate, terminal, onNext, onBack } = props;
+
     return (
         <div className={styles.wrapper}>
-            <Button onClick={props.onNext} className={styles.button} variant="success">
-                Next
+            {(intermediate || terminal) && (
+                <Button onClick={onBack} className={styles.back} variant="primary">
+                    Back
+                </Button>
+            )}
+            <Button onClick={onNext} className={styles.next} variant="success">
+                {terminal ? "Play" : "Next"}
             </Button>
         </div>
     );
--- src/tests/components/layout/wizard/play/PlayWizardFooter.test.tsx
+++ src/tests/components/layout/wizard/play/PlayWizardFooter.test.tsx
@@ -1,18 +1,47 @@
-import { fireEvent, render } from "@testing-library/react";
-import PlayWizardFooter from "../../../../../components/layout/wizard/play/PlayWizardFooter";
+import { fireEvent, render, screen } from "@testing-library/react";
+import PlayWizardFooter, { PlayWizardFooterProps } from "../../../../../components/layout/wizard/play/PlayWizardFooter";
 
 const onNextHandler = jest.fn();
+const onBackHandler = jest.fn();
 
-const setup = () => {
-    const component = render(<PlayWizardFooter onNext={onNextHandler} />);
-    return {
-        next: component.getByText('Next'),
-        ...component
+let props: PlayWizardFooterProps;
+
+beforeEach(() => {
+    props = {
+        onNext: onNextHandler,
+        onBack: onBackHandler,
+        terminal: false,
+        intermediate: false
     }
-}
+});
 
 test('Clicking the next button should call the onNext event handler', () => {
-    const { next } = setup();
-    fireEvent.click(next);
+    const component = render(<PlayWizardFooter {...props} />)
+    fireEvent.click(component.getByText('Next'));
     expect(onNextHandler).toHaveBeenCalled();
+});
+
+test('Clicking the back button should call the onBack event handler', () => {
+    props.intermediate = true;
+    const component = render(<PlayWizardFooter {...props} />)
+    fireEvent.click(component.getByText('Back'));
+    expect(onBackHandler).toHaveBeenCalled();
+});
+
+test('Passing terminal as true should render a back button', () => {
+   props.terminal = true;
+   const component = render(<PlayWizardFooter {...props} />)
+   expect(component.getByText('Back')).toBeInTheDocument();
+});
+
+test('Passing intermediate as true should render a back button', () => {
+   props.intermediate = true;
+   const component = render(<PlayWizardFooter {...props} />)
+   expect(component.getByText('Back')).toBeInTheDocument();
+});
+
+test('Passing terminal as true should render the next button text as "Play"', () => {
+   props.terminal = true;
+   const component = render(<PlayWizardFooter {...props} />)
+   expect(component.getByText('Play')).toBeInTheDocument();
 });
