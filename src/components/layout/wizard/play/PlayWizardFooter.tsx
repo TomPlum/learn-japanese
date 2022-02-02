2f246f2cdd3d94fc1d6e19edd87075f82e2d9f12
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2022-02-02 19:55:56 +0000

    JPUI-3 | Integrated PlayWizardProgress w/PlayWizardFooter Component

--- src/components/layout/wizard/play/PlayWizardFooter.tsx
+++ src/components/layout/wizard/play/PlayWizardFooter.tsx
@@ -1,17 +1,21 @@
 import { Button } from "react-bootstrap";
 import styles from "../../../../styles/sass/components/layout/wizard/play/PlayWizardFooter.module.scss";
+import PlayWizardProgress from "./PlayWizardProgress";
 
 export interface PlayWizardFooterProps {
+    custom: boolean;
+    currentStage: number;
     intermediate?: boolean;
     terminal?: boolean;
     onNext: () => void;
     onBack: () => void;
     onPlay: () => void;
+    onChangeStage: (stage: number) => void;
 }
 
 const PlayWizardFooter = (props: PlayWizardFooterProps) => {
 
-    const { intermediate, terminal, onNext, onBack, onPlay } = props;
+    const { custom, intermediate, terminal, currentStage, onNext, onBack, onPlay, onChangeStage } = props;
 
     return (
         <div className={styles.wrapper}>
@@ -21,6 +25,13 @@ const PlayWizardFooter = (props: PlayWizardFooterProps) => {
                         Back
                     </Button>
                 )}
+            </div>
+            <div>
+                <PlayWizardProgress
+                    custom={custom}
+                    stage={currentStage}
+                    onSelectStage={onChangeStage}
+                />
             </div>
             <div>
                 <Button onClick={terminal ? onPlay : onNext} className={styles.next} variant="success">
--- src/tests/components/layout/wizard/play/PlayWizardFooter.test.tsx
+++ src/tests/components/layout/wizard/play/PlayWizardFooter.test.tsx
@@ -1,47 +1,59 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react";
 import PlayWizardFooter, { PlayWizardFooterProps } from "../../../../../components/layout/wizard/play/PlayWizardFooter";
 
 const onNextHandler = jest.fn();
 const onBackHandler = jest.fn();
+const onPlayHandler = jest.fn();
+const onChangeStageHandler = jest.fn();
 
 let props: PlayWizardFooterProps;
 
 beforeEach(() => {
     props = {
+        custom: false,
+        currentStage: 0,
+        onPlay: onPlayHandler,
         onNext: onNextHandler,
         onBack: onBackHandler,
+        onChangeStage: onChangeStageHandler,
         terminal: false,
         intermediate: false
     }
 });
 
 test('Clicking the next button should call the onNext event handler', () => {
-    const component = render(<PlayWizardFooter {...props} />)
+    const component = render(<PlayWizardFooter {...props} />);
     fireEvent.click(component.getByText('Next'));
     expect(onNextHandler).toHaveBeenCalled();
 });
 
 test('Clicking the back button should call the onBack event handler', () => {
     props.intermediate = true;
-    const component = render(<PlayWizardFooter {...props} />)
+    const component = render(<PlayWizardFooter {...props} />);
     fireEvent.click(component.getByText('Back'));
     expect(onBackHandler).toHaveBeenCalled();
 });
 
 test('Passing terminal as true should render a back button', () => {
    props.terminal = true;
-   const component = render(<PlayWizardFooter {...props} />)
+   const component = render(<PlayWizardFooter {...props} />);
    expect(component.getByText('Back')).toBeInTheDocument();
 });
 
 test('Passing intermediate as true should render a back button', () => {
    props.intermediate = true;
-   const component = render(<PlayWizardFooter {...props} />)
+   const component = render(<PlayWizardFooter {...props} />);
    expect(component.getByText('Back')).toBeInTheDocument();
 });
 
 test('Passing terminal as true should render the next button text as "Play"', () => {
    props.terminal = true;
-   const component = render(<PlayWizardFooter {...props} />)
+   const component = render(<PlayWizardFooter {...props} />);
    expect(component.getByText('Play')).toBeInTheDocument();
+});
+
+test('Selecting a stage from the progress component should call the onChangeStage handler', () => {
+    const component = render(<PlayWizardFooter {...props} />);
+    fireEvent.click(component.getByTitle('Topic'));
+    expect(onChangeStageHandler).toHaveBeenLastCalledWith(0);
 });
