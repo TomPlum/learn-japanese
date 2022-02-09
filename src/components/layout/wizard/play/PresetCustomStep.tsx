a7c5d377d853a2b34e016c8bb0ae6d9142ec75f5
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2022-02-09 17:48:50 +0000

    JPUI-3 | Improved PresetCustom Step Button Styling

--- src/components/layout/wizard/play/PresetCustomStep.tsx
+++ src/components/layout/wizard/play/PresetCustomStep.tsx
@@ -18,23 +18,26 @@ const PresetCustomStep = (props: PresetCustomStepProps) => {
     const presetButtonClass = isCustom ? styles.button : styles.selected;
     const customButtonClass = isCustom ? styles.selected : styles.button;
 
+    const hammerIconClass = [styles.icon, styles.custom].join(" ");
+    const presetIconClass = [styles.icon, styles.preset].join(" ");
+
     useEffect(() => {
         onSelect(isCustom);
     }, [isCustom]);
 
     return (
-        <div>
+        <div className={styles.wrapper}>
             <Container fluid className={styles.container}>
-                <Row className={styles.wrapper}>
+                <Row>
                     <Col>
                         <Button onClick={() => setIsCustom(false)} className={presetButtonClass}>
-                            <FontAwesomeIcon icon={faProjectDiagram} fixedWidth className={styles.icon} />
+                            <FontAwesomeIcon icon={faProjectDiagram} fixedWidth className={presetIconClass} />
                             <p className={styles.text}>Preset</p>
                         </Button>
                     </Col>
                     <Col>
                         <Button onClick={() => setIsCustom(true)} className={customButtonClass}>
-                            <FontAwesomeIcon icon={faHammer} fixedWidth className={styles.icon} />
+                            <FontAwesomeIcon icon={faHammer} fixedWidth className={hammerIconClass} />
                             <p className={styles.text}>Custom</p>
                         </Button>
                     </Col>
