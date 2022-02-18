8a84485cf872b297621d91867e03e2f6b284e9ff
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2022-02-18 20:42:54 +0000

    JPUI-7 | Implemented Generic Dashboard Card Component & Prototyped Dashboard Layout

---
+++ src/components/layout/DashboardCard.tsx
@@ -0,0 +1,83 @@
+import { PropsWithChildren, useState } from "react";
+import styles from "../../styles/sass/components/layout/DashboardCard.module.scss";
+import { Fade } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSyncAlt } from "@fortawesome/free-solid-svg-icons";
+
+export interface DashboardCardProps {
+    size?: "sm" | "md" | "lg";
+    title?: string;
+    loading?: boolean;
+    error?: string;
+    className?: string;
+    onReload?: () => void;
+}
+
+const DashboardCard = (props: PropsWithChildren<DashboardCardProps>) => {
+
+    const { title, error, size, loading, onReload, children, className } = props;
+
+    const getSize = (): string | undefined => {
+        switch (size) {
+            case "sm": return "300px";
+            case "md": return "400px";
+            case "lg": return "550px";
+            default: return undefined;
+        }
+    }
+
+    const containerClasses = [styles.container, className];
+    if (error) containerClasses.push(styles.containerError)
+
+    return (
+        <div className={containerClasses.join(" ")} style={{ height: getSize() }}>
+            {title && (
+                <div className={styles.header}>
+                    <span className={styles.title}>{title}</span>
+                    {error && (
+                        <FontAwesomeIcon
+                            title="Retry"
+                            icon={faSyncAlt}
+                            onClick={onReload}
+                            className={styles.reload}
+                        />
+                    )}
+                </div>
+            )}
+            {loading && (
+                <Fade in={loading} appear>
+                    <div className={styles.loading}>
+                        <div className={styles.shimmer}>
+                            <div className={styles["_2iwr"]}/>
+                            <div className={styles["_2iws"]}/>
+                            <div className={styles["_2iwt"]}/>
+                            <div className={styles["_2iwu"]}/>
+                            <div className={styles["_2iwv"]}/>
+                            <div className={styles["_2iww"]}/>
+                            <div className={styles["_2iwx"]}/>
+                            <div className={styles["_2iwy"]}/>
+                            <div className={styles["_2iwz"]}/>
+                            <div className={styles["_2iw-"]}/>
+                            <div className={styles["_2iw_"]}/>
+                            <div className={styles["_2ix0"]}/>
+                        </div>
+                    </div>
+                </Fade>
+            )}
+
+            {!loading && !error && (
+                <div className={styles.content}>
+                    {children}
+                </div>
+            )}
+
+            {!loading && error && (
+                <div className={styles.errorContainer}>
+                    <span className={styles.error}>{error}</span>
+                </div>
+            )}
+        </div>
+    );
+}
+
+export default DashboardCard;
--- src/components/pages/MainMenuPage.tsx
+++ src/components/pages/MainMenuPage.tsx
@@ -1,11 +1,51 @@
 import styles from "../../styles/sass/components/pages/MainMenuPage.module.scss";
-import CardContainer from "../cards/CardContainer";
+import { Col, Container, Row } from "react-bootstrap";
+import DashboardCard from "../layout/DashboardCard";
 
 const MainMenuPage = () => {
     return (
-        <div className={styles.wrapper}>
-            <CardContainer />
-        </div>
+        <Container className={styles.wrapper}>
+            <Row className={styles.row}>
+                <Col>
+                    <DashboardCard className={styles.toolbar} />
+                </Col>
+            </Row>
+            <Row className={styles.row}>
+                <Col md={3}>
+                    <DashboardCard size="sm" title="Profile">
+
+                    </DashboardCard>
+                    <DashboardCard size="sm" title="Settings">
+
+                    </DashboardCard>
+                </Col>
+
+                <Col>
+                    <DashboardCard size="md" title="Play" loading>
+
+                    </DashboardCard>
+
+                    <DashboardCard size="md" title="Activity" error="Something went wrong.">
+
+                    </DashboardCard>
+                </Col>
+
+                <Col md={3}>
+                    <DashboardCard size="sm" title="Highscores">
+
+                    </DashboardCard>
+
+                    <DashboardCard size="sm" title="Mistakes">
+
+                    </DashboardCard>
+
+                    <DashboardCard size="sm" title="Stats">
+
+                    </DashboardCard>
+                </Col>
+            </Row>
+            {/*<CardContainer />*/}
+        </Container>
     );
 }
 
