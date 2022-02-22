11aecd748c0080cc8f5cf497099bf553204774bb
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2022-02-22 16:14:34 +0000

    JPUI-3 | Added TopicSelectionStep Component Test Suite

--- src/tests/components/layout/wizard/play/TopicSelectionStep.test.tsx
+++ src/tests/components/layout/wizard/play/TopicSelectionStep.test.tsx
@@ -1,1 +1,18 @@
-test.todo("Write these once we know what's going on with it.");
+import { fireEvent, render } from "@testing-library/react";
+import TopicSelectionStep from "../../../../../components/layout/wizard/play/TopicSelectionStep";
+import Topic from "../../../../../domain/Topic";
+
+const onSelectHandler = jest.fn();
+
+test('Should render all the topics', () => {
+    const component = render(<TopicSelectionStep onSelect={onSelectHandler} />);
+    Topic.ALL.forEach(topic => {
+        expect(component.getByText(topic.name)).toBeInTheDocument();
+    });
+});
+
+test('Should call the onSelect event handler when clicking a topic', () => {
+    const component = render(<TopicSelectionStep onSelect={onSelectHandler} />);
+    fireEvent.click(component.getByText('Days & Months'));
+    expect(onSelectHandler).toHaveBeenLastCalledWith(Topic.CALENDAR);
+});
