9bf0b7e0f08f96b23439f84dc8b297c015bc2729
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2022-04-13 17:07:58 +0100

    JPUI-26 | Added Icon Component Test Coverage

---
+++ src/tests/components/ui/menu/icon/Icon.test.tsx
@@ -0,0 +1,21 @@
+import { fireEvent, render } from "@testing-library/react";
+import Icon from "../../../../../components/ui/menu/icon/Icon";
+
+const onClickEventHandler = jest.fn();
+
+test('Should render the icon with a title equal to the name with the Fa prefix removed', () => {
+    const component = render(<Icon value="FaAccessibleIcon" />);
+    expect(component.getByTitle('AccessibleIcon')).toBeInTheDocument();
+});
+
+test('Should call the onClick event handler with clicking the icon', () => {
+    const { container } = render(<Icon value="FaAccessibleIcon" onClick={onClickEventHandler} />);
+    fireEvent.click(container.firstChild!);
+    expect(onClickEventHandler).toHaveBeenCalled();
+});
+
+test('Should pass the given class name to the icon', () => {
+    const { container } = render(<Icon value="FaAccessibleIcon" className="myTestClass" />);
+    const icon = container.firstChild!;
+    expect(icon).toHaveClass('myTestClass');
+});
