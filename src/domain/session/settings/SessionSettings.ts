ea2aa5ba154c11a5a984c23fa846a5dd2af05189
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2022-04-17 15:38:52 +0100

    JPUI-22 | Exposed SessionSettings "From Preset" Static Factory Constructor & Added Test Coverage

--- src/domain/session/settings/SessionSettings.ts
+++ src/domain/session/settings/SessionSettings.ts
@@ -1,6 +1,9 @@
 import DataSettings from "./data/DataSettings";
 import GameSettings from "./game/GameSettings";
 import LearnSettings from "./LearnSettings";
+import SessionMode from "../SessionMode";
+import PlayMode from "../PlayMode";
+import LearnMode from "../LearnMode";
 
 export class SessionSettings {
     private readonly _gameSettings: GameSettings | undefined;
@@ -21,8 +24,22 @@ export class SessionSettings {
         return new SessionSettings(dataSettings, undefined, settings);
     }
 
+    public static fromPreset(preset: SessionMode) {
+        if (preset instanceof PlayMode) {
+            return SessionSettings.forGame(preset.dataSettings, preset.modeSettings as GameSettings);
+        } else if (preset instanceof LearnMode) {
+            return SessionSettings.forLearning(preset.dataSettings, preset.modeSettings as LearnSettings);
+        } else {
+            throw new Error("Unknown preset type");
+        }
+    }
+
     get gameSettings(): GameSettings | undefined {
         return this._gameSettings;
+    }
+
+    get learnSettings(): LearnSettings | undefined {
+        return this._learnSettings;
     }
 
     get dataSettings(): DataSettings {
