258a48a79e4dd729e5a169f8cdb8f3abbd4dbbe5
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2022-04-17 15:20:31 +0100

    JPUI-3 | Extracted SessionSettingsSummary Component From ConfirmationStep

--- src/components/layout/wizard/steps/ConfirmationStep.tsx
+++ src/components/layout/wizard/steps/ConfirmationStep.tsx
@@ -1,16 +1,15 @@
 import { SessionSettings } from "../../../../domain/session/settings/SessionSettings";
-import styles from "../../../../styles/sass/components/layout/wizard/steps/ConfirmationStep.module.scss";
 import { Accordion, Button } from "react-bootstrap";
 import { faDownload } from "@fortawesome/free-solid-svg-icons";
-import QuestionType from "../../../../domain/game/QuestionType";
 import CustomPresetForm from "../form/CustomPresetForm";
 import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { WizardStep } from "../SessionWizard";
+import SessionSettingsSummary from "../../../settings/SessionSettingsSummary";
 
 export interface ConfirmationStepProps {
     settings: SessionSettings;
-    onSelectStage: (stage: WizardStep) => void;
+    onSelectStage?: (stage: WizardStep) => void;
 }
 
 const ConfirmationStep = (props: ConfirmationStepProps) => {
@@ -18,61 +17,6 @@ const ConfirmationStep = (props: ConfirmationStepProps) => {
 
     const [inSavePresetForm, setInSavePresetForm] = useState(false);
     const [showSave, setShowSave] = useState(true);
-
-    const gameSettings = settings.gameSettings;
-    const dataSettings = settings.dataSettings;
-
-    if (!gameSettings) {
-        return <span>Your game settings could not be found. Please reload and try again.</span>
-    }
-
-    const time = gameSettings.time;
-    const hints = gameSettings.hints.quantity;
-    const hasUnlimitedHints = gameSettings.hints.unlimited
-
-    const questionType = gameSettings?.question.type;
-    const cards = gameSettings.question.cards;
-    const lives = gameSettings.lives.quantity;
-
-    const getQuestionTypeWord = (): string => {
-        switch (questionType) {
-            case QuestionType.TEXT: return "type";
-            case QuestionType.MATCH: return "match";
-            case QuestionType.CHOICE: return `pick from ${cards} cards`;
-            case QuestionType.AUDIO: return "listen to";
-            default: return "answer";
-        }
-    }
-
-    const getTimeWords = () => {
-        if (time?.timed) {
-            return (
-                <span>{"your session will be "}
-                    <span className={[styles.time, styles.highlight].join(" ")}>{"timed"}</span>
-                </span>
-            );
-        }
-
-        if (time?.countdown) {
-            return (
-                <span>{"you'll have "}
-                    <span className={[styles.time, styles.highlight].join(" ")}>
-                        {time?.secondsPerQuestion}{" seconds per question"}
-                    </span>
-                </span>
-            );
-        }
-
-        return <span>{"you'll have "}<span className={styles.time}>no time restrictions</span></span>;
-    }
-
-    const getScoreWords = (): string => {
-        if (gameSettings?.question.score) {
-            return "your score will be tracked";
-        } else {
-            return "scoring will be disabled";
-        }
-    }
 
     const handleClickSave = () => {
         setShowSave(false);
@@ -84,53 +28,9 @@ const ConfirmationStep = (props: ConfirmationStepProps) => {
         setInSavePresetForm(false);
     }
 
-    const answerField = gameSettings?.question.answerField.name;
-    const questionField = gameSettings?.question.questionField.name;
-    const livesDisplay = lives > 0 ? lives : "unlimited";
-    const hintsDisplay = hasUnlimitedHints ? "unlimited" : hints > 0 ? hints : "no";
-
-    const { TOPIC, QUESTION, LIVES, HINT, TIME, DATA } = WizardStep;
-
     return (
         <div>
-            <p className={styles.question}>
-                <span>{"You'll be given the "}</span>
-                <span className={[styles.field, styles.highlight].join(" ")} onClick={() => onSelectStage(QUESTION)}>
-                    {questionField}
-                </span>
-                <span>{" and must "}</span>
-                <span className={[styles.type, styles.highlight].join(" ")} onClick={() => onSelectStage(QUESTION)}>
-                    {getQuestionTypeWord()}
-                </span>
-                <span>{" the "}</span>
-                <span className={[styles.field, styles.highlight].join(" ")} onClick={() => onSelectStage(QUESTION)}>
-                    {answerField}
-                </span>
-                <span>{" for "}</span>
-                <span className={[styles.quantity, styles.highlight].join(" ")} onClick={() => onSelectStage(DATA)}>
-                    {dataSettings?.quantity}
-                </span>
-                <span>{" questions about "}</span>
-                <span className={[styles.topic, styles.highlight].join(" ")} onClick={() => onSelectStage(TOPIC)}>
-                    {dataSettings?.topic.name}
-                </span>
-                <span>{". "}</span>
-                <span>{"You'll have "}</span>
-                <span className={[styles.lives, styles.highlight].join(" ")} onClick={() => onSelectStage(LIVES)}>
-                    {livesDisplay}{" lives"}
-                </span>
-                <span>{", "}</span>
-                <span className={[styles.hint, styles.highlight].join(" ")} onClick={() => onSelectStage(HINT)}>
-                    {hintsDisplay}{" hints"}
-                </span>
-                <span>{", "}</span>
-                <span onClick={() => onSelectStage(TIME)}>{getTimeWords()}</span>
-                <span>{" and "}</span>
-                <span className={[styles.score, styles.highlight].join(" ")} onClick={() => onSelectStage(QUESTION)}>
-                    {getScoreWords()}
-                </span>
-                <span>{"."}</span>
-            </p>
+           <SessionSettingsSummary settings={settings} onSelectStage={onSelectStage} />
 
             <Accordion>
                 {showSave && (
