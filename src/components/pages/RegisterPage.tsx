e678fddb6c691a3fe6d86127dabfa3f315b69570
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2022-05-29 19:15:21 +0100

    JPUI-34 | Removed RegisterPage redundant description text

--- src/components/pages/RegisterPage.tsx
+++ src/components/pages/RegisterPage.tsx
@@ -2,9 +2,6 @@ import { useUserSelector } from "../../hooks";
 import { Redirect, useHistory } from "react-router-dom";
 import RegistrationForm from "../user/RegistrationForm";
 import styles from "../../styles/sass/components/pages/RegisterPage.module.scss";
-import { Alert } from "react-bootstrap";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faInfoCircle } from "@fortawesome/free-solid-svg-icons";
 
 const RegisterPage = () => {
     const user = useUserSelector(state => state.user).user;
@@ -21,13 +18,6 @@ const RegisterPage = () => {
     return (
         <div className={styles.wrapper}>
             <div className={styles.form}>
-                <Alert variant="info" className={styles.info}>
-                    <FontAwesomeIcon icon={faInfoCircle} className={styles.icon} />
-                    <span>
-                        Creating an account allows you to create and save custom presets,
-                        compete in the high-scores, and track your progress and statistics.
-                    </span>
-                </Alert>
                 <RegistrationForm onSuccess={handleRegistrationSuccess}/>
             </div>
         </div>
