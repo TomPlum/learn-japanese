86e623683bb7ca3d50d2e358e1ba6beb15a277c9
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2022-06-15 19:43:28 +0100

    JPUI-10 | Added notification settings placeholder examples

--- src/components/settings/modal/NotificationsSettingsTab.tsx
+++ src/components/settings/modal/NotificationsSettingsTab.tsx
@@ -1,4 +1,7 @@
 import SettingsTabTitle from "./SettingsTabTitle";
+import styles from "../../../styles/sass/components/settings/modal/LearnSettingsTab.module.scss";
+import SettingsButton from "./SettingsButton";
+import { faCopy, faEraser, faFireAlt } from "@fortawesome/free-solid-svg-icons";
 
 const NotificationsSettingsTab = () => {
     return (
@@ -7,6 +10,22 @@ const NotificationsSettingsTab = () => {
                 title="Notification Settings"
                 description="Configure settings related to your notification centre and app-wide communication."
             />
+
+            <div className={styles.section}>
+                <p className={styles.heading}>Streak Notifications</p>
+                <p className={styles.text}>
+                    Toggle notifications for significant streak milestones.
+                </p>
+                <SettingsButton name="Enable" icon={faFireAlt} />
+            </div>
+
+            <div className={styles.section}>
+                <p className={styles.heading}>Outstanding Mistakes Reminder</p>
+                <p className={styles.text}>
+                    Toggle reminders for outstanding mistakes that need reviewing.
+                </p>
+                <SettingsButton name="Enable" icon={faEraser} />
+            </div>
         </div>
     );
 }
