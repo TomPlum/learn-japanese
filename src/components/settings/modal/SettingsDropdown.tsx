266060989039e09e541c780cf0c1acc4eaec618a
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2022-06-17 17:33:00 +0100

    JPUI-10 | SettingsDropdown - Removed redundant ref object and improved styling

--- src/components/settings/modal/SettingsDropdown.tsx
+++ src/components/settings/modal/SettingsDropdown.tsx
@@ -2,7 +2,7 @@ import styles from "../../../styles/sass/components/settings/modal/SettingsDropd
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronDown, faChevronUp, IconDefinition } from "@fortawesome/free-solid-svg-icons";
 import { useRef, useState } from "react";
-import { Overlay, Popover } from "react-bootstrap";
+import { Overlay } from "react-bootstrap";
 
 export interface SettingsDropdownOption {
     name: string;
@@ -18,10 +18,9 @@ export interface SettingsDropdownProps {
 const SettingsDropdown = (props: SettingsDropdownProps) => {
     const { id, options, onChange } = props;
 
-    const [selected, setSelected] = useState(options[0]);
-    const ref = useRef(null);
-    const targetRef = useRef(null);
+    const target = useRef(null);
     const [show, setShow] = useState(false);
+    const [selected, setSelected] = useState(options[0]);
 
     const handleChange = (option: SettingsDropdownOption) => {
         onChange(option.name);
@@ -29,24 +28,28 @@ const SettingsDropdown = (props: SettingsDropdownProps) => {
         setShow(false);
     }
 
+    const chevronClasses = [styles.chevron];
+    if (show) chevronClasses.push(styles.inspect);
+
     return (
-        <div ref={ref} >
-            <div className={styles.button} onClick={() => setShow(true)} ref={targetRef} title="Click to see options">
+        <>
+            <div ref={target} className={styles.button} onClick={() => setShow(true)} title="Click to see options">
                 <FontAwesomeIcon icon={selected.icon} className={styles.icon} />
                 <span className={styles.name}>{selected.name}</span>
-                <span className={styles.chevron}><FontAwesomeIcon icon={show ? faChevronUp : faChevronDown} /></span>
+                <span className={chevronClasses.join(" ")}>
+                    <FontAwesomeIcon icon={show ? faChevronUp : faChevronDown} />
+                </span>
             </div>
 
             <Overlay
                 show={show}
                 rootClose={true}
-                target={targetRef}
                 placement="bottom"
-                container={ref.current}
+                target={target.current}
                 onHide={() => setShow(false)}
             >
                 <div className={styles.menu}>
-                    {options.map((option: SettingsDropdownOption) => (
+                    {options.filter(it => it.name != selected.name).map((option: SettingsDropdownOption) => (
                         <div key={option.name} className={styles.option} onClick={() => handleChange(option)}>
                             <FontAwesomeIcon icon={option.icon} className={styles.icon} />
                             <span>{option.name}</span>
@@ -54,7 +57,7 @@ const SettingsDropdown = (props: SettingsDropdownProps) => {
                     ))}
                 </div>
             </Overlay>
-        </div>
+        </>
     );
 }
 
