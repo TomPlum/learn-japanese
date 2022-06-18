684bf0b52c29d355517b26e9976e0f085247ab11
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2022-06-18 14:13:27 +0100

    JPUI-10 | Added GeneralSettingsTab Activity Feed Preference

--- src/components/settings/modal/GeneralSettingsTab.tsx
+++ src/components/settings/modal/GeneralSettingsTab.tsx
@@ -1,12 +1,36 @@
 import SettingsTabTitle from "./SettingsTabTitle";
+import styles from "../../../styles/sass/components/settings/modal/InterfaceSettingsTab.module.scss";
+import SettingsDropdown from "./SettingsDropdown";
+import { faRss } from "@fortawesome/free-solid-svg-icons";
 
 const GeneralSettingsTab = () => {
+    const handleActivityFeedChange = (quantity: string) => {
+
+    }
+
     return (
         <div data-testid="general-settings-tab">
             <SettingsTabTitle
                 title="General Settings"
                 description="General settings for common elements and actions around the app."
             />
+
+            <div className={styles.section}>
+                <p className={styles.heading}>Activity Feed</p>
+                <p className={styles.text}>
+                    Set the number of events to display in the activity feed card by default.
+                </p>
+                <SettingsDropdown
+                    buttonIcon={faRss}
+                    onChange={handleActivityFeedChange}
+                    id="general-settings-activity-selector"
+                    options={[
+                        { name: '3' },
+                        { name: '5' },
+                        { name: '8' }
+                    ]}
+                />
+            </div>
         </div>
     );
 }
