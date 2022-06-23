a2d65e79489c3fa7b847c17bfc988df51aa39449
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2022-06-23 16:58:19 +0100

    JPUI-10 | Fixed Failing InterfaceSettingsTab Tests

--- src/tests/components/settings/modal/InterfaceSettingsTab.test.tsx
+++ src/tests/components/settings/modal/InterfaceSettingsTab.test.tsx
@@ -1,24 +1,15 @@
 import { fireEvent, render } from "@testing-library/react";
 import InterfaceSettingsTab from "../../../../components/settings/modal/InterfaceSettingsTab";
 
+const onEditDashboardLayoutHandler = jest.fn();
+
 test('Should render the main tab contents on initial render', () => {
-    const component = render(<InterfaceSettingsTab />);
+    const component = render(<InterfaceSettingsTab onEditDashboardLayout={onEditDashboardLayoutHandler} />);
     expect(component.getByTestId('interface-settings-tab')).toBeInTheDocument();
 });
 
-test('Should render the dashboard layout editor after clicking the button', () => {
-    // Should initially render the settings tab and not the editor
-    const component = render(<InterfaceSettingsTab />);
-    expect(component.getByTestId('interface-settings-tab')).toBeInTheDocument();
-    expect(component.queryByTestId('dashboard-layout-editor')).not.toBeInTheDocument();
-
-    // Opening the editor should stop rendering the tab contents
+test('Should call the onEditDashboard event handler after clicking the button', () => {
+    const component = render(<InterfaceSettingsTab onEditDashboardLayout={onEditDashboardLayoutHandler} />);
     fireEvent.click(component.getByText('Open Layout Editor'));
-    expect(component.queryByTestId('interface-settings-tab')).not.toBeInTheDocument();
-    expect(component.getByTestId('dashboard-layout-editor')).toBeInTheDocument();
-
-    // Dismissing the editor should stop rendering it and re-render the interface settings
-    fireEvent.click(component.getByTitle('Close'))
-    expect(component.queryByTestId('dashboard-layout-editor')).not.toBeInTheDocument();
-    expect(component.getByTestId('interface-settings-tab')).toBeInTheDocument();
+    expect(onEditDashboardLayoutHandler).toHaveBeenCalled();
 });
