873aae69da113fc8bb565308e5aae602a9c69fb1
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2022-07-08 17:33:48 +0100

    JPUI-10 | Added GeneralSettingsTab Test Suite

--- src/tests/components/settings/modal/GeneralSettingsTab.test.tsx
+++ src/tests/components/settings/modal/GeneralSettingsTab.test.tsx
@@ -1,1 +1,14 @@
-test.todo("Write these once added settings to this tab");
+import renderReduxConsumer from "../../../renderReduxConsumer";
+import GeneralSettingsTab from "../../../../components/settings/modal/GeneralSettingsTab";
+import { fireEvent, screen } from "@testing-library/react";
+
+test('Should render the activity activity feed quantity settings dropdown', async () => {
+    const component = renderReduxConsumer(<GeneralSettingsTab />);
+
+    fireEvent.click(component.getByTestId('general-settings-activity-selector'));
+
+    // Should render the correct options
+    expect(await screen.findByText('3')).toBeInTheDocument();
+    expect(await screen.findByText('5')).toBeInTheDocument();
+    expect(await screen.findByText('8')).toBeInTheDocument();
+});
