0b9615d8f53735d6501712ce5af580fc3cc996f5
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2022-07-31 17:13:29 +0100

    JPUI-36 | Fixed i18n translation config

--- src/i18n.ts
+++ src/i18n.ts
@@ -6,31 +6,31 @@ import { initReactI18next } from "react-i18next";
 // or even better, manage them separated from your code: https://react.i18next.com/guides/multiple-translation-files)
 const resources = {
     en: {
-        landing: {
-            button: {
-                learn: "Learn"
+        translation: {
+            landing: {
+                button: {
+                    "learn": "Learn"
+                }
             }
         }
     },
     jp: {
-        landing: {
-            button: {
-                learn: "習う"
+        translation: {
+            landing: {
+                button: {
+                    "learn": "習う"
+                }
             }
         }
     }
 };
 
-i18n
-    .use(initReactI18next) // passes i18n down to react-i18next
+i18n.use(initReactI18next)
     .init({
         resources,
-        lng: "en", // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
-        // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
-        // if you're using a language detector, do not define the lng option
-
+        lng: "en",
         interpolation: {
-            escapeValue: false // react already safes from xss
+            escapeValue: false
         }
     });
 
