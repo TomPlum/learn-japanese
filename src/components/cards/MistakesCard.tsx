09178d6430dd1becb368aa66dc9a5b1806e25d95
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2022-08-02 12:32:21 +0100

    JPUI-36 | Added MistakesCard Japanese translations

--- src/components/cards/MistakesCard.tsx
+++ src/components/cards/MistakesCard.tsx
@@ -3,35 +3,37 @@ import DashboardCardHeader from "../layout/card/DashboardCardHeader";
 import DashboardCardLink from "../layout/card/DashboardCardLink";
 import { faEraser } from "@fortawesome/free-solid-svg-icons";
 import styles from "../../styles/sass/components/cards/MistakesCard.module.scss";
+import { useTranslation } from "react-i18next";
 
 const MistakesCard = () => {
 
-    const mistakes = 17;
+    const { t, ready } = useTranslation("translation", { keyPrefix: "dashboard.card.mistakes" });
 
+    const mistakes = 17;
     const colourClass = mistakes > 20 ? styles.red : mistakes > 10 ? styles.orange : styles.green;
 
     return (
-        <DashboardCard size="sm" className={styles.card}>
+        <DashboardCard size="sm" className={styles.card} loading={!ready}>
             <DashboardCard.Header>
                 <DashboardCardHeader.Title>
-                    Mistakes
+                    {t("title")}
                 </DashboardCardHeader.Title>
             </DashboardCard.Header>
 
             <DashboardCard.Body className={styles.body}>
-                <div className={[styles.quantity, colourClass].join(" ")} title="Practice">
+                <div className={[styles.quantity, colourClass].join(" ")} title={t("practice")}>
                     {mistakes}
                 </div>
-                <p className={styles.desc}>to review</p>
+                <p className={styles.desc}>{t("review")}</p>
                 <div className={styles.types}>
-                    <div className={styles.type} title="Kanji">字</div>
-                    <div className={styles.type} title="Kana">あ</div>
-                    <div className={styles.type} title="Definitions">A</div>
+                    <div className={styles.type} title={t("kanji")}>字</div>
+                    <div className={styles.type} title={t("kana")}>あ</div>
+                    <div className={styles.type} title={t("definitions")}>A</div>
                 </div>
             </DashboardCard.Body>
 
             <DashboardCard.Footer className={styles.footer}>
-                <DashboardCardLink text="View Mistakes" icon={faEraser} className={styles.practice} />
+                <DashboardCardLink text={t("view")} icon={faEraser} className={styles.practice} />
             </DashboardCard.Footer>
         </DashboardCard>
     );
