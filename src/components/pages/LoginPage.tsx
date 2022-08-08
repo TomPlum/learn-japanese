1d7cecb1a1c2963e37fc6f8461ffc46567f7f65f
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2022-08-08 18:27:37 +0100

    JPUI-36 | Added Japanese translations for the login page session expiry message

--- src/components/pages/LoginPage.tsx
+++ src/components/pages/LoginPage.tsx
@@ -2,8 +2,12 @@ import { useQueryParams, useUserSelector } from "../../hooks";
 import { Redirect, useHistory } from "react-router-dom";
 import LoginForm from "../user/LoginForm";
 import styles from "../../styles/sass/components/pages/LoginPage.module.scss";
+import { useTranslation } from "react-i18next";
+import LoadingSpinner from "../ui/loading/LoadingSpinner";
 
 const LoginPage = () => {
+
+    const { t, ready } = useTranslation("translation", { keyPrefix: "forms.login" });
     const user = useUserSelector(state => state.user).user;
     const history = useHistory();
     const params = useQueryParams();
@@ -15,11 +19,12 @@ const LoginPage = () => {
     return (
         <div className={styles.wrapper}>
             <div className={styles.form}>
-                <LoginForm
+                <LoadingSpinner active={!ready} variant="success" className={styles.spinner} />
+                {ready && <LoginForm
                     username={params.get("username") ?? ""}
                     onSuccess={() => history.push("/home")}
-                    info={params.get("session-expired") ? "Your session has expired. Please log-in again." : undefined}
-                />
+                    info={params.get("session-expired") ? t("session-expired") : undefined}
+                />}
             </div>
         </div>
     );
