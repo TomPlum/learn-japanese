86c1188e70f7f3fe0360ddc9ad7d8d2fa65d6a1b
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2022-11-14 11:33:12 +0000

    JPUI-43 | Genki Chapter 18 - Part 1 (Table Content)

--- src/components/pages/genki/GenkiChapter18.tsx
+++ src/components/pages/genki/GenkiChapter18.tsx
@@ -16,13 +16,17 @@ const GenkiChapter18 = () => {
                     describe changes that happen to things or people (intransitive verbs).
                 </p>
                 <GenkiTable chapter={18}>
-                   <thead>
-                        <th></th>
-                        <th>Transitive</th>
-                        <th>Intransitive</th>
-                        <th></th>
-                   </thead>
+                  {/* <thead>
+                        <th colSpan={2}>1</th>
+                        <th className={styles.genkiTwo}>Transitive</th>
+                        <th colSpan={2} className={styles.genkiTwo}>Intransitive</th>
+                        <th>2</th>
+                   </thead>*/}
                     <tbody>
+                       <tr>
+                           <td colSpan={2} className={styles.genkiTwo}>Transitive</td>
+                           <td colSpan={2} className={styles.genkiTwo}>Intransitive</td>
+                       </tr>
                        <tr>
                            <td>開ける</td>
                            <td>open something</td>
@@ -42,7 +46,50 @@ const GenkiChapter18 = () => {
                             <td>something goes inside</td>
                         </tr>
                         <tr>
-
+                            <td>出す</td>
+                            <td>take something out</td>
+                            <td>出る</td>
+                            <td>something goes out</td>
+                        </tr>
+                        <tr>
+                            <td>つける</td>
+                            <td>turn something on</td>
+                            <td>つく</td>
+                            <td>something goes on</td>
+                        </tr>
+                        <tr>
+                            <td>消す</td>
+                            <td>turn something off;</td>
+                            <td>消える</td>
+                            <td>something goes off</td>
+                        </tr>
+                        <tr>
+                            <td>&nbsp;</td>
+                            <td colSpan={3}>extinguish something</td>
+                        </tr>
+                        <tr>
+                            <td>壊す</td>
+                            <td>breaking something</td>
+                            <td>壊れる</td>
+                            <td>something breaks</td>
+                        </tr>
+                        <tr>
+                            <td>汚す</td>
+                            <td>make something dirty</td>
+                            <td>汚れる</td>
+                            <td>something becomes dirty</td>
+                        </tr>
+                        <tr>
+                            <td>落とす</td>
+                            <td>drop something</td>
+                            <td>落ちる</td>
+                            <td>something drops</td>
+                        </tr>
+                        <tr>
+                            <td>沸かす</td>
+                            <td>boil water</td>
+                            <td>沸く</td>
+                            <td>water boils</td>
                         </tr>
                     </tbody>
                 </GenkiTable>
