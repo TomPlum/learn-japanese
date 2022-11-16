4772e562834b8f293444348459760681b888146a
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2022-11-16 17:47:45 +0000

    JPUI-43 | Added FuriganaDisplay.test.tsx

---
+++ src/tests/components/ui/display/FuriganaDisplay.test.tsx
@@ -0,0 +1,25 @@
+import {render} from "@testing-library/react";
+import FuriganaDisplay from "../../../../components/ui/display/FuriganaDisplay";
+import {getByTextWithMarkup} from "../../../Queries";
+
+test("Should render each of the given characters with furigana at the bottom", () => {
+    render(<FuriganaDisplay
+        chars={[
+            { kanji: '窓', kana: 'まど' },
+            { pre: 'が', kanji: '開', kana: 'あ', okurigana: 'い', post: 'ています。' }
+        ]}
+        position='bottom'
+    />);
+    expect(getByTextWithMarkup('窓まどが開あいています。')).toBeInTheDocument();
+});
+
+test("Should render each of the given characters with furigana at the top", () => {
+    render(<FuriganaDisplay
+        chars={[
+            { kanji: '窓', kana: 'まど' },
+            { pre: 'が', kanji: '開', kana: 'あ', okurigana: 'い', post: 'ています。' }
+        ]}
+        position='top'
+    />);
+    expect(getByTextWithMarkup('窓まどが開あいています。')).toBeInTheDocument();
+});
