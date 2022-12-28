da5ab874ea55f0a2ea4b75f001d4e4fec6fea0c3
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2022-12-28 09:19:20 +0000

    JPUI-46 | Added Learn API MSW Handlers

--- src/mocks/browser.ts
+++ src/mocks/browser.ts
@@ -3,9 +3,11 @@ import { setupWorker } from 'msw'
 import { handlers as user } from './handlers/user.handlers'
 import { handlers as kanji } from './handlers/kanji.handlers'
 import { handlers as presets } from './handlers/presets.handlers'
+import { handlers as learn } from './handlers/learn.handlers'
 
 export const worker = setupWorker(
     ...user,
     ...kanji,
-    ...presets
+    ...presets,
+    ...learn
 )
