5e82a8883dcbaa2ec7044e7c55504b7d5b41f8ce
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2022-12-28 09:02:22 +0000

    JPUI-46 | Added Presets API MSW Handlers

--- src/mocks/browser.ts
+++ src/mocks/browser.ts
@@ -2,8 +2,10 @@ import { setupWorker } from 'msw'
 
 import { handlers as user } from './handlers/user.handlers'
 import { handlers as kanji } from './handlers/kanji.handlers'
+import { handlers as presets } from './handlers/presets.handlers'
 
 export const worker = setupWorker(
     ...user,
-    ...kanji
+    ...kanji,
+    ...presets
 )
