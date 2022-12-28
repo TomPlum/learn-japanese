83c26b71a1a9500c90cdb89a79c53ff4252bd145
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2022-12-28 19:52:05 +0000

    JPUI-46 | Fixed Kana API MSW Handlers

--- src/mocks/handlers/kana.handlers.ts
+++ src/mocks/handlers/kana.handlers.ts
@@ -2,7 +2,7 @@ import {rest} from "msw";
 import {api} from "../util";
 
 export const handlers = [
-    rest.post(`${api}/kana/all`, (req, res, ctx) => {
+    rest.get(`${api}/kana/all`, (req, res, ctx) => {
         return res(
             ctx.status(200),
             ctx.json({
