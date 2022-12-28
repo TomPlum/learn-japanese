a0c8f96f4a33aa05affd7d8a827be129a46825da
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2022-12-28 19:51:21 +0000

    JPUI-46 | Added Kana API MSW Handlers

--- src/mocks/browser.ts
+++ src/mocks/browser.ts
@@ -5,11 +5,13 @@ import { handlers as kanji } from './handlers/kanji.handlers'
 import { handlers as presets } from './handlers/presets.handlers'
 import { handlers as learn } from './handlers/learn.handlers'
 import { handlers as highScores } from './handlers/high-scores.handlers'
+import { handlers as kana } from './handlers/kana.handlers'
 
 export const worker = setupWorker(
     ...user,
     ...kanji,
     ...presets,
     ...learn,
-    ...highScores
+    ...highScores,
+    ...kana
 )
