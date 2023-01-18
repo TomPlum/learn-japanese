bc32b97cd1680e55e12d596c4239eb9cae031113
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2023-01-18 18:38:23 +0000

    JPUI-47 | Turned off non-null and optional assertions

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,8 @@ module.exports = {
     "extends": [
         "eslint:recommended",
         "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended"
+        "plugin:@typescript-eslint/recommended",
+        "plugin:react/jsx-runtime"
     ],
     "overrides": [
     ],
@@ -20,5 +21,7 @@ module.exports = {
         "@typescript-eslint"
     ],
     "rules": {
+        "@typescript-eslint/no-non-null-assertion": "off",
+        "@typescript-eslint/no-non-null-asserted-optional-chain": "off",
     }
 }
