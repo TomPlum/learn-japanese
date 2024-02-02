4f4dd95b7994fcbc060fb9fc188e3c94f198029a
Thomas Plumpton <Thomas.Plumpton@hotmail.co.uk>
2024-02-02 18:07:43 +0000

    JPUI-51 | Fixed more failing tests after new hook integration

--- src/components/ui/display/KanjiWordDisplay/KanjiWordDisplay.test.tsx
+++ src/components/ui/display/KanjiWordDisplay/KanjiWordDisplay.test.tsx
@@ -1,42 +1,27 @@
-import { fireEvent, render, screen } from "@testing-library/react"
+import { fireEvent, screen, waitFor } from "@testing-library/react";
 import KanjiWordDisplay  from "./KanjiWordDisplay"
-import { Kanji } from "types/kanji/Kanji"
-import { KanjiReading } from "types/kanji/KanjiReading"
-import { ReadingType } from "types/kanji/ReadingType"
-import { KyoikuGrade } from "types/kanji/KyoikuGrade"
-import JLTPLevel from "types/learn/JLTPLevel"
+import { render } from "__test-utils__";
+import { server } from "__test-utils__/msw.ts";
+import {
+  useGetKanjiByCharacterHandlers
+} from "api/hooks/kanji/useGetKanjiByCharacter/useGetKanjiByCharacter.handlers.ts";
 
-const mockKanjiRepository = vi.fn()
-vi.mock("repository/KanjiRepository", () => ({
-  default: function () {
-    return { getByValue: mockKanjiRepository }
-  }
-}))
-
-const kanji = new Kanji(
-  "鳥",
-  [new KanjiReading("tori", "とり", ReadingType.KUN), new KanjiReading("chou", "ちょう", ReadingType.ON)],
-  ["bird"],
-  KyoikuGrade.TWO,
-  JLTPLevel.N5,
-  "",
-  [],
-  10,
-  ["animal"]
-)
+beforeEach(() => {
+  server.use(...useGetKanjiByCharacterHandlers)
+})
 
 test("Should render the character if it does not match a known Kanji character", () => {
-  const component = render(<KanjiWordDisplay value="き" />)
+  const { component } = render(<KanjiWordDisplay value="き" />)
   expect(component.getByText("き")).toBeInTheDocument()
 })
 
 test("Should render the character as Inspectable if it is a known Kanji character", async () => {
-  mockKanjiRepository.mockResolvedValue(kanji)
-  const component = render(<KanjiWordDisplay value="鳥" />)
+  const { component } = render(<KanjiWordDisplay value="小" />)
 
-  fireEvent.mouseOver(component.getByText("鳥"))
+  fireEvent.mouseOver(component.getByText("小"))
 
-  expect(await screen.findByTestId("鳥-information")).toBeInTheDocument()
-  expect(mockKanjiRepository).toHaveBeenCalledWith("鳥")
-  expect(screen.getByText("bird")).toBeInTheDocument()
+  expect(await screen.findByTestId("小-information")).toBeInTheDocument()
+  await waitFor(() => {
+    expect(screen.getByText("small, little")).toBeInTheDocument()
+  })
 })
